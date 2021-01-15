b20fbe01ae95274c529c6d3248dec0d5df49c3aa
Adam Etzion <adametzion90@gmail.com>
2021-01-14 19:39:39 -0500

    Update admin invoice show_spec for user story 7 re: total_revenue

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -54,6 +54,8 @@ describe 'Admin Invoices Index Page' do
   end
 
   it 'should display the total revenue the invoice will generate' do
+    @discount_a = BulkDiscount.create!(name: "Going Out of Business", discount: 0.2, threshold: 10, merchant: @merchant1)
+
     expect(page).to have_content("Total Revenue: $#{@i1.total_revenue}")
 
     expect(page).to_not have_content(@i2.total_revenue)
