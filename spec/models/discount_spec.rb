8e51f3fe8a42f6363b34eddf4174641eec4788b1
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-04 20:21:02 -0700

    Test for validations for numericality

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Discount, type: :model do
   describe "validations" do
     it {should validate_presence_of :percent_discount}
     it {should validate_presence_of :quantity}
+    it {should validate_numericality_of :percent_discount}
+    it {should validate_numericality_of :quantity}
   end
   describe "relationships" do
     it {should belong_to :merchant}
