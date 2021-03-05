17a5e08dbf1c875bd503bd273d018d54fdbf37e8
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-05 15:26:02 -0700

    Add create action as well as private #discount_create_params

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,12 +1,29 @@
 class DiscountsController < ApplicationController
-  before_action :find_merchant, only: [:index]
+  before_action :find_merchant, only: [:index, :new, :create]
 
   def index
+  end
+
+  def new
+  end
+
+  def create
+    @discount = @merchant.discounts.new(discount_create_params)
+    if @discount.save
+      redirect_to merchant_discounts_path(@merchant)
+    else
+      flash[:error] = "Please use only whole numbers in Percent Discount and Quantity fields"
+      render :new
+    end
   end
 
   private
 
   def find_merchant
     @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def discount_create_params
+    params.permit(:percent_discount, :quantity)
   end
 end
