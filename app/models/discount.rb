d3d67d600c9e09cd56a2568e418e06ed4c045242
Jake Volpe <volpe.jacob@gmail.com>
2021-03-06 11:42:44 -0700

    Add controller method and model validation for creating discounts, pass tests

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,6 +1,9 @@
 class Discount < ApplicationRecord
   validates_presence_of :quantity,
                         :percentage
+                        
+  validates :quantity, numericality: true
+  validates :percentage, numericality: true
   
   belongs_to :merchant
 end
