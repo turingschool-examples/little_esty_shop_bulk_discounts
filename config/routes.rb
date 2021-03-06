6b58aed1a21062b64200e1bfd9801038d8a5b6da
Brisa Garcia <brisagarciaglz@gmail.com>
2021-03-05 18:19:48 -0700

    Add bulk discount routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :items, except: [:destroy]
     resources :item_status, only: [:update]
     resources :invoices, only: [:index, :show, :update]
+    resources :bulk_discounts
+
   end
 
   namespace :admin do
