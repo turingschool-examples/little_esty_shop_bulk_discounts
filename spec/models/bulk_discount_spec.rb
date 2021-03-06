5259b878a6239883e12f2f969876b77b2c8b6814
Brisa Garcia <brisagarciaglz@gmail.com>
2021-03-06 10:24:31 -0700

    Add basic tests

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
-  #   describe 'validations' do
-  #   it { should validate_presence_of :percentage_discount}
-  #   it { should validate_presence_of :quantity_threshold}
-  # end
+    describe 'validations' do
+    it { should validate_presence_of :percent_discount}
+    it { should validate_presence_of :quantity_threshold}
+  end
 
   describe 'relationships' do
     it {should belong_to :merchant}
