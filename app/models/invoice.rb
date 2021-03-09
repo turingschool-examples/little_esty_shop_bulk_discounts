24106ad453cdd42a8ad549bfcbc4dc1593093055
kylejschulz <kylschulz@gmail.com>
2021-03-08 22:29:44 -0500

    minor changes

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,4 @@
+
 class Invoice < ApplicationRecord
   validates_presence_of :status,
                         :customer_id
@@ -13,4 +14,36 @@ class Invoice < ApplicationRecord
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+
+
+  # def eligible_for_discount
+  #   invoice_items
+  #   .joins(:bulk_discounts)
+  #   .where('invoice_items.quantity >= ?', discounts.quantity_threshold)
+  #   .select(quantity * invoice_item.unit_price * (1 - percent_discount/100) as discounted_revenue)
+  # end
+
+  def find_best_discount(invoice_item)
+    #return the total and the bulk_id
+    eligible_for_discount
+
+    .where('invoice_items.id = ?', invoice_item.id)
+    .order('discounted_revenue')
+    .limit(1)
+  end
+  #
+  # def price_with_discount(discount_id)
+  #
+  #
+  # end
+  #
+  # def total_revenue
+  #   invoice_items.sum do |item|
+  #     item.find_best_discount(item).discounted_revenue
+  #   end
+  # end
+  #invoice.items.joins(merchant: :discounts)
+
+
 end
