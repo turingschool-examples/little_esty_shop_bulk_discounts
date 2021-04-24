cde1c7449d98419111bedfc9c8646d83fd3380bf
koshea9 <keegan.oshea9@gmail.com>
2021-04-24 17:25:39 -0600

    Add delete button to merchant index page

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -17,6 +17,15 @@ class BulkDiscountsController < ApplicationController
     bulk_discount = merchant.bulk_discounts.create(bulk_discount_params)
     redirect_to merchant_bulk_discounts_path(merchant)
   end
+
+  def destroy
+    merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = BulkDiscount.find(params[:id])
+
+    merchant.bulk_discounts.delete(bulk_discount)
+
+    redirect_to merchant_bulk_discounts_path(merchant)
+  end
 end
 
 private
