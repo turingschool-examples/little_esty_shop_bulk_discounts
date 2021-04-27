d9b6bf5f416468a1259094cf94c9f2db8e12664f
koshea9 <keegan.oshea9@gmail.com>
2021-04-27 15:45:57 -0600

    refactor invoice items methods and test

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
+  before(:each) do
+    @merchant1 = Merchant.create!(name: 'Hair Care')
+    @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+    @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+    @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2,  created_at: "2012-03-27 14:54:09")
+    @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 2, created_at: "2012-03-27 14:54:09")
+
+    @bulk_discount_2 = BulkDiscount.create!(name: '30% off', percentage_discount: 30, quantity_threshold: 15, merchant_id: @merchant1.id)
+    @bulk_discount_1 = BulkDiscount.create!(name: '20% off', percentage_discount: 20, quantity_threshold: 10, merchant_id: @merchant1.id)
+  end
+
   describe "validations" do
     it { should validate_presence_of :invoice_id }
     it { should validate_presence_of :item_id }
@@ -17,27 +28,14 @@ RSpec.describe InvoiceItem, type: :model do
 
   describe "instance methods" do
     describe "#revenue_no_discount_applied" do
-      it "can calculate revenue for one item" do
-        @merchant1 = Merchant.create!(name: 'Hair Care')
-        @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
-        @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
-        @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2,  created_at: "2012-03-27 14:54:09")
-        @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2, created_at: "2012-03-27 14:54:09")
+      it "can calculate revenue for one item with no discount applied" do
 
-        expect(@ii_1.revenue_no_discount_applied).to eq(90)
+        expect(@ii_1.revenue_no_discount_applied).to eq(100)
       end
     end
 
     describe "#discount_applied" do
       it 'can calculate correct discount applied' do
-        @merchant1 = Merchant.create!(name: 'Hair Care')
-        @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
-        @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
-        @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2,  created_at: "2012-03-27 14:54:09")
-        @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 2, created_at: "2012-03-27 14:54:09")
-
-        @bulk_discount_2 = BulkDiscount.create!(name: '30% off', percentage_discount: 30, quantity_threshold: 15, merchant_id: @merchant1.id)
-        @bulk_discount_1 = BulkDiscount.create!(name: '20% off', percentage_discount: 20, quantity_threshold: 10, merchant_id: @merchant1.id)
 
         expect(@ii_1.discount_applied).to eq(@bulk_discount_1)
       end
@@ -45,17 +43,16 @@ RSpec.describe InvoiceItem, type: :model do
 
     describe "revenue_with_discount_applied" do
       it 'can calculate revenue with correct discounts applied' do
-      @merchant1 = Merchant.create!(name: 'Hair Care')
-      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
-      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
-      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2,  created_at: "2012-03-27 14:54:09")
-      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 2, created_at: "2012-03-27 14:54:09")
-
-      @bulk_discount_2 = BulkDiscount.create!(name: '30% off', percentage_discount: 30, quantity_threshold: 15, merchant_id: @merchant1.id)
-      @bulk_discount_1 = BulkDiscount.create!(name: '20% off', percentage_discount: 20, quantity_threshold: 10, merchant_id: @merchant1.id)
 
       expect(@ii_1.revenue_with_discount_applied).to eq(80)
+      end
     end
+
+    describe "total_discount" do
+      it 'can calculate the total discount applied' do
+
+        expect(@ii_1.total_discount).to eq(20)
+      end
     end
   end
 end
