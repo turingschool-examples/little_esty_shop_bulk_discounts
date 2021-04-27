93d4c1489f40e8fa33a20ec006b5f9b2aeed96a7
koshea9 <keegan.oshea9@gmail.com>
2021-04-27 08:25:20 -0600

    Add validation, tests and custom messages

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,4 +1,7 @@
 class BulkDiscount < ApplicationRecord
+  validates :name, presence: true, exclusion: { in: %w(Kyle kyle), message: "Nice, try. Don't put this on Kyle! He doesn't even work here!" }
+  validates :percentage_discount, numericality: true, inclusion: { in: ( 1..99 ), message: "Please enter a number 1-99" }
+  validates :quantity_threshold, numericality: true
   belongs_to :merchant
   has_many :items, through: :merchant
   has_many :invoice_items, through: :items
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -7,4 +7,35 @@ RSpec.describe BulkDiscount, type: :model do
     it { should have_many(:invoice_items).through(:items) }
     it { should have_many(:invoices).through(:invoice_items) }
   end
+
+  describe 'validations' do
+
+    it { should validate_presence_of(:name)}
+    it { should validate_numericality_of(:percentage_discount)}
+    it { should validate_numericality_of(:quantity_threshold)}
+
+    it 'validates name' do
+      merchant1 = Merchant.create!(name: 'Hair Care')
+      bulk_discount = BulkDiscount.new
+      bulk_discount.name = ''
+      bulk_discount.validate
+      expect(bulk_discount.errors[:name]).to include("can't be blank")
+    end
+
+    it 'validates it is not kyle' do
+      merchant1 = Merchant.create!(name: 'Hair Care')
+      bulk_discount = BulkDiscount.new
+      bulk_discount.name = 'kyle'
+      bulk_discount.validate
+      expect(bulk_discount.errors[:name]).to include("Nice, try. Don't put this on Kyle! He doesn't even work here!")
+    end
+
+    it 'validates it is a number between 1 and 99' do
+      merchant1 = Merchant.create!(name: 'Hair Care')
+      bulk_discount = BulkDiscount.new
+      bulk_discount.percentage_discount = 101
+      bulk_discount.validate
+      expect(bulk_discount.errors[:percentage_discount]).to include("Please enter a number 1-99")
+    end
+  end
 end
