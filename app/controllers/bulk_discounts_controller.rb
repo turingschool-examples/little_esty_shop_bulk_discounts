2f9cb2140b92cc6928ae79d1fdca705f4d69b18e
koshea9 <keegan.oshea9@gmail.com>
2021-04-27 08:56:05 -0600

    Add error message for new bulk discount form

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -29,7 +29,13 @@ class BulkDiscountsController < ApplicationController
   def create
     merchant = Merchant.find(params[:merchant_id])
     bulk_discount = merchant.bulk_discounts.create(bulk_discount_params)
-    redirect_to merchant_bulk_discounts_path(merchant)
+
+    if bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(merchant)
+    else
+      flash[:notice] = "#{error_message(bulk_discount.errors)}"
+      redirect_to new_merchant_bulk_discount_path(merchant)
+    end
   end
 
   def destroy
