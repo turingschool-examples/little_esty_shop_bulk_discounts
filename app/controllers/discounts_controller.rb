4975cbefea8f8ef4c9faebc71fb29b2df749aef2
Zach T <ztrokey@gmail.com>
2021-06-12 12:59:55 -0600

    add discount create

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,4 +1,6 @@
 class DiscountsController < ApplicationController
+  before_action :find_merchant, only: [:new, :create, :index]
+
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.discounts
@@ -11,6 +13,21 @@ class DiscountsController < ApplicationController
   end
 
   def new
-    # require 'pry'; binding.pry
+
+  end
+
+  def create
+    Discount.create!(percentage_discount: params[:percentage_discount].to_d,
+                     quantity_threshold: params[:quantity_threshold],
+                     merchant_id: params[:merchant_id])
+
+    redirect_to merchant_discounts_path(@merchant)
+
+  end
+
+  private
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
