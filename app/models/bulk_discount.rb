ffd794f89cc58cbf3e1a0268383749400ff83e79
Weston Ellis <ellisweston112@gmail.com>
2021-09-24 10:25:29 -0600

    Write validations

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,4 +1,6 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
-
+  validates_numericality_of :discount, greater_than: 0
+  validates_numericality_of :discount, less_than_or_equal_to: 100
+  validates_numericality_of :threshold, greater_than: 0
 end
