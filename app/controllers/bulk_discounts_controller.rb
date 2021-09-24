00da979b2919d055cf7204b8e353d6c11998b75f
Weston Ellis <ellisweston112@gmail.com>
2021-09-24 15:18:40 -0600

    Write new bulk discount form/page

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -10,6 +10,21 @@ class BulkDiscountsController < ApplicationController
   end
 
   def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.new
+  end
 
+  def create
+    merchant= Merchant.find(params[:merchant_id])
+    merchant.bulk_discounts.create!(bulk_discount_params)
+
+    redirect_to merchant_bulk_discounts_path(merchant)
+  end
+
+
+  private
+
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:discount, :threshold)
   end
 end
