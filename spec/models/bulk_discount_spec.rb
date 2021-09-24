7a5022e4167773b3f88d212d681941a14ca7fada
Weston Ellis <ellisweston112@gmail.com>
2021-09-24 10:23:08 -0600

    Write validation tests

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe BulkDiscount do
   describe "relationships" do
     it { should belong_to(:merchant) }
   end
+
+  describe 'validations' do
+    it { should validate_numericality_of(:discount).is_less_than_or_equal_to(100)}
+    it { should validate_numericality_of(:discount).is_greater_than(0)}
+    it { should validate_numericality_of(:threshold).is_greater_than(0)}
+  end
 end
