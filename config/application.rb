e8020e28da6f28d75959cbf000369196e5a82442
Ted S <tstaros23@gmail.com>
2021-09-24 15:55:07 -0600

    Change config

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module LittleEtsyShop
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     # config/application.rb
-    
+
+
+    config.assets.js_compressor = :uglifier
   end
 end
