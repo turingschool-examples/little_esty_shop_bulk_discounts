2e7c7cb7c88c325f7b1bc0d729b80883e9388636
Ted S <tstaros23@gmail.com>
2021-09-24 14:55:29 -0600

    Add config

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module LittleEtsyShop
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
   end
 end
