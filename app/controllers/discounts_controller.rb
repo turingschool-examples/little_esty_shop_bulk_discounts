254da8481851194f0f581f0513c1ee8c9421176d
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-09-25 21:05:19 -0600

    Finish discounts index user story

---
+++ app/controllers/discounts_controller.rb
@@ -0,0 +1,9 @@
+class DiscountsController < ApplicationController
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def show
+    @discount = Discount.find(params[:id])
+  end
+end
--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -119,4 +119,10 @@ RSpec.describe 'merchant dashboard' do
   it "shows the date that the invoice was created in this format: Monday, July 18, 2019" do
     expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %-d, %Y"))
   end
+
+  it 'shows a link to view all merchants discounts' do
+    expect(page).to have_content("View discounts")
+    click_link("View discounts")
+    expect(current_path).to eq(merchant_discounts_path(@merchant1))
+  end
 end
