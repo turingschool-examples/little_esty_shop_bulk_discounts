6075dd1e3b7bdb892ecbfb16685d6e9c4cec11f5
Ted Staros <tstaros23@gmail.com>
2021-11-14 23:19:36 -0700

    Work in progress

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,10 +7,20 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :discounts, through: :merchants
 
   enum status: [:cancelled, 'in progress', :complete]
 
   def total_revenue
     invoice_items.sum("unit_price * quantity")
+  end
+
+  def find_discount_percentages
+    wip = discounts.joins(:invoice_items)
+    .where()
+    .order(percentage_discount: :desc)
+    .distinct
+    .pluck(:percentage_discount)
+    require "pry"; binding.pry
   end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -23,5 +23,39 @@ RSpec.describe Invoice, type: :model do
 
       expect(@invoice_1.total_revenue).to eq(100)
     end
+  describe "bulk discount methods" do
+    before(:each) do
+        @merchant1 = Merchant.create!(name: 'Hair Care')
+        @merchant2 = Merchant.create!(name: 'Target')
+
+        @discount = Discount.create!(quantity_threshold: 7, percentage_discount: 15, merchant_id: @merchant1.id)
+        @discount2 = Discount.create!(quantity_threshold: 10, percentage_discount: 30, merchant_id: @merchant1.id)
+        @discount3 = Discount.create!(quantity_threshold: 15, percentage_discount: 25, merchant_id: @merchant1.id)
+
+        @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+
+        @invoice_A = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+
+        @item_A = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+        @ii_A = InvoiceItem.create!(invoice_id: @invoice_A.id, item_id: @item_A.id, quantity: 12, unit_price: 10, status: 2)
+        #84
+        @item_B = Item.create!(name: "Comb", description: "This cleans your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+        @ii_B = InvoiceItem.create!(invoice_id: @invoice_A.id, item_id: @item_B.id, quantity: 5, unit_price: 10, status: 2)
+        #50
+        @item_C = Item.create!(name: "Broom", description: "This cleans your house", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+        @ii_C = InvoiceItem.create!(invoice_id: @invoice_A.id, item_id: @item_C.id, quantity: 15, unit_price: 10, status: 2)
+        #105
+        @item_D = Item.create!(name: "Mop", description: "This cleans your floor", unit_price: 10, merchant_id: @merchant2.id, status: 1)
+        @ii_D = InvoiceItem.create!(invoice_id: @invoice_A.id, item_id: @item_D.id, quantity: 20, unit_price: 10, status: 2)
+        #200
+      end
+      it "can find the total bulk discount revenue" do
+        expect(@invoice_A.total_revenue).to eq(520)
+        expect(@invoice_A.total_bulk_discount_revenue).to eq(439)
+      end
+      it "can find the discount percentage for an invoice item, if any" do
+        expect(@invoice_A.find_discount_percentages).to eq([30, 25, 15])
+      end
+    end
   end
 end
