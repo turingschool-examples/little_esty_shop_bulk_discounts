f9b66bda3c576a593152c9c162dbb5c02c81968f
John Napier <jcee.napier@gmail.com>
2022-01-14 19:14:55 -0700

    complete user story 1

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,5 +1,13 @@
 class BulkDiscountsController < ApplicationController 
   def index 
+    if params[:merchant_id].present?
+      @bulk_discounts = BulkDiscount.where(merchant_id: params[:merchant_id])
+    else 
+      @bulk_discounts = BulkDiscount.all
+    end 
+  end
+
+  def show 
     
   end
 end
