3cf37f31d1f37cd504232818b1f242b328bc00d4
Russell Rockwood <russell.rockwood@gmail.com>
2022-01-15 11:01:31 -0500

    add bulk discounts table

---
+++ db/migrate/20220115155024_create_bulk_discounts.rb
@@ -0,0 +1,9 @@
+class CreateBulkDiscounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bulk_discounts do |t|
+      t.integer :percentage
+      t.integer :threshold
+      t.timestamps
+    end
+  end
+end
--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -119,4 +119,8 @@ RSpec.describe 'merchant dashboard' do
   it "shows the date that the invoice was created in this format: Monday, July 18, 2019" do
     expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %-d, %Y"))
   end
+
+  it "links to bulk discounts index page" do
+
+  end
 end
