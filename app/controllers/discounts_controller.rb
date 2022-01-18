c9f47e96a7598d01b8a2717530b05ec37f68fd12
Katy Harrod <m.catherine.harrod@gmail.com>
2022-01-17 20:23:15 -0700

    Pass create a new discount spec

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -11,4 +11,20 @@ class DiscountsController < ApplicationController
   def new
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    discount = @merchant.discounts.create(discount_params)
+    redirect_to merchant_discounts_path
+  end
+
+  # def create
+  #   discount = Discount.create(discount_params)
+  #   redirect_to merchant_discounts_path
+  # end
+
+private
+  def discount_params
+    params.permit(:name, :min_quantity, :percent_off)
+  end
 end
