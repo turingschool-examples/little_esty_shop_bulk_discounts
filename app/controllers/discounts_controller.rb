5ca1ea0c934e42905639d1b26d68c1c631518c18
Katy Harrod <m.catherine.harrod@gmail.com>
2022-01-18 14:46:40 -0700

    Pass discount destroy spec

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -15,7 +15,14 @@ class DiscountsController < ApplicationController
   def create
     @merchant = Merchant.find(params[:merchant_id])
     discount = @merchant.discounts.create(discount_params)
-    redirect_to merchant_discounts_path
+    redirect_to merchant_discounts_path(@merchant)
+  end
+
+  def destroy
+    merchant = Merchant.find(params[:merchant_id])
+    discount = Discount.find(params[:id])
+    discount.destroy
+    redirect_to "/merchant/#{merchant.id}/discounts"
   end
 
 private
--- spec/features/discounts/destroy_spec.rb
+++ spec/features/discounts/destroy_spec.rb
@@ -8,7 +8,12 @@ describe 'delete discount' do
     @preserved_discount2 = @merchant1.discounts.create!(name: "saved 2", percent_off: 20, min_quantity: 5)
     visit merchant_discounts_path(@merchant1)
   end
+
   it 'has link to delete discount' do
-    click_link "Delete this discount"
+    within "#discount-#{@deleted_discount.id}" do
+      click_link "Delete this discount"
+    end
+      expect(current_path).to eq("/merchant/#{@merchant1.id}/discounts")
+      expect(page).not_to have_content(@deleted_discount.name)
   end
 end
