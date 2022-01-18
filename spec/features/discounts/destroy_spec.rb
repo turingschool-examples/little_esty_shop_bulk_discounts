2e72efd1fb9514259a79ad5b7e2894d413f1e935
Katy Harrod <m.catherine.harrod@gmail.com>
2022-01-17 20:41:06 -0700

    Initial spec testing for destroy discount link

---
+++ spec/features/discounts/destroy_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'delete discount' do
+  before do
+    @merchant1 = Merchant.create!(name: 'Hair Care')
+    @deleted_discount = @merchant1.discounts.create!(name: "nul", percent_off: 100, min_quantity: 1)
+    @preserved_discount1 = @merchant1.discounts.create!(name: "saved", percent_off: 10, min_quantity: 3)
+    @preserved_discount2 = @merchant1.discounts.create!(name: "saved 2", percent_off: 20, min_quantity: 5)
+    visit merchant_discounts_path(@merchant1)
+  end
+  it 'has link to delete discount' do
+    click_link "Delete this discount"
+  end
+end
