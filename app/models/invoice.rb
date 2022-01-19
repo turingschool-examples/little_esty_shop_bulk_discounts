4c19a15e47caa4c89272b953710aab114b980c8a
Russell Rockwood <russell.rockwood@gmail.com>
2022-01-19 15:19:19 -0500

    revert back to previous

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -16,7 +16,8 @@ class Invoice < ApplicationRecord
   end
 
   def discounted_revenue
-    total_discount =  bulk_discounts
+    total_discount =  merchants
+                      .joins(:bulk_discounts)
                       .where('invoice_items.quantity >= bulk_discounts.threshold')
                       .select('invoice_items.*')
                       .group('invoice_items.item_id')
@@ -24,7 +25,6 @@ class Invoice < ApplicationRecord
                       .pluck(1)
                       .sum
 
-                      
     if total_discount == 0
       return 'No Discount Applied'
     else
