1cd38779985fe97cde4e4b388388b2f1eff989ff
John Napier <jcee.napier@gmail.com>
2022-01-18 19:10:31 -0700

    heroku API deployment

--- app/poros/holiday_service.rb
+++ app/poros/holiday_service.rb
@@ -1,9 +1,9 @@
-# require 'httparty'
+require 'httparty'
 
-# class HolidayService 
+class HolidayService 
 
-#   def united_states
-#     response = HTTParty.get("https://date.nager.at/api/v3/NextPublicHolidays/US")
-#     JSON.parse(response.body, symboilize_names: true)
-#   end
-# end
+  def united_states
+    response = HTTParty.get("https://date.nager.at/api/v3/NextPublicHolidays/US")
+    JSON.parse(response.body, symboilize_names: true)
+  end
+end
