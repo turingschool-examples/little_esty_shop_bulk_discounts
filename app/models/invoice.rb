c5a4dd2658d4a11019dd04ab54cecfb1abbac5e1
Oscar Santos <oscar303data@gmail.com>
2022-03-03 18:56:06 -0700

    Finish basic set up

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,7 +8,7 @@ class Invoice < ApplicationRecord
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
 
-  enum status: [:cancelled, 'in progress', :completed]
+  enum status: [:cancelled, 'in progress', :complete]
 
   def total_revenue
     invoice_items.sum("unit_price * quantity")
---
+++ db/migrate/20220304013451_create_bulk_discounts.rb
@@ -0,0 +1,10 @@
+class CreateBulkDiscounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bulk_discounts do |t|
+      t.float :discount
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
