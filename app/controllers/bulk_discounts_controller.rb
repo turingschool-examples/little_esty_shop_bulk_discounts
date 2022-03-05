80522e0a4717fbca118568e31a663292b626c117
Oscar Santos <oscar303data@gmail.com>
2022-03-05 09:16:16 -0700

    Add destroy method in boulk discount controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -14,8 +14,13 @@ class BulkDiscountsController < ApplicationController
   def create
     bulk_discount = params[:bulk_discount]
     @merchant = Merchant.find(params[:merchant_id])
-    #bd = BulkDiscount.create(bulk_discount[:discount], bulk_discount[:quantity])
     BulkDiscount.create(discount: bulk_discount[:discount], quantity: bulk_discount[:quantity], merchant: @merchant)
+    redirect_to merchant_bulk_discounts_path
+  end
+
+  def destroy
+    @merchant = Merchant.find_by(id: params[:merchant_id])
+    BulkDiscount.find(params[:id]).destroy
     redirect_to merchant_bulk_discounts_path
   end
 end
