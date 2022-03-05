7911eae4291f3e31e941f13dc5dbe3c49b1ab2f9
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-05 13:17:52 -0700

    Feat: MVC for discounts index, test passes

---
+++ app/controllers/discounts_controller.rb
@@ -0,0 +1,5 @@
+class DiscountsController < ApplicationController
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+end
--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -120,3 +120,16 @@ RSpec.describe 'merchant dashboard' do
     expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %-d, %Y"))
   end
 end
+RSpec.describe 'merchant dashboard' do
+  describe 'Bulk Discounts' do
+    it 'displays a link to view all discounts' do
+      merchant = create(:merchant)
+      visit "/merchant/#{merchant.id}/dashboard"
+      save_and_open_page
+
+      expect(page).to have_link("Discounts")
+      click_link "Discounts"
+      expect(current_path).to eq("/merchant/#{merchant.id}/discounts")
+    end
+  end
+end
