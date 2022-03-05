15e495c1ce067e9763868b16af561fa2794792c6
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-03-05 12:05:49 -0700

    Commit - need to switch to branch

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -13,6 +13,20 @@ describe "merchant bulk discount show" do
   it "shows percentage and quantity thresholds" do
     expect(page).to have_content(@discount1.percent_discount)
     expect(page).to have_content(@discount1.qty_threshold)
-    save_and_open_page
   end
+
+  it "shows link to edit discount" do
+    click_link "Edit Discount"
+    expect(current_path).to eq()
+  end
+
+# As a merchant
+# When I visit my bulk discount show page
+# Then I see a link to edit the bulk discount
+# When I click this link
+# Then I am taken to a new page with a form to edit the discount
+# And I see that the discounts current attributes are pre-poluated in the form
+# When I change any/all of the information and click submit
+# Then I am redirected to the bulk discount's show page
+# And I see that the discount's attributes have been updated
 end
