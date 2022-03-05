50909203021d705aca68f79d5dd1cffe977eec91
Oscar Santos <oscar303data@gmail.com>
2022-03-04 21:53:49 -0700

    Create method new and create

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -5,4 +5,17 @@ class BulkDiscountsController < ApplicationController
 
   def show
   end
+
+  def new
+    @discount = BulkDiscount.new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    bulk_discount = params[:bulk_discount]
+    @merchant = Merchant.find(params[:merchant_id])
+    #bd = BulkDiscount.create(bulk_discount[:discount], bulk_discount[:quantity])
+    BulkDiscount.create(discount: bulk_discount[:discount], quantity: bulk_discount[:quantity], merchant: @merchant)
+    redirect_to merchant_bulk_discounts_path
+  end
 end
