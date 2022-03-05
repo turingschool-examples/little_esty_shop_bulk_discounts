f2dbcd413321c15c44bef75208c5dea58d7158ed
Oscar Santos <oscar303data@gmail.com>
2022-03-04 21:59:06 -0700

    Create test for new discount

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'Bulk discounts Index Page' do
       expect(page).to have_content(discount_1.quantity)
       expect(page).to have_content(discount_2.discount)
       expect(page).to have_content(discount_2.quantity)
-      
+
   end
 
   it 'displays link to view all my discounts' do
@@ -31,6 +31,39 @@ RSpec.describe 'Bulk discounts Index Page' do
 
       expect(current_path).to eq("/merchant/#{merchant.id}/bulk_discounts/#{discount_1.id}")
 
+  end
+
+  it 'displays link to create a new discount' do
+      merchant = Merchant.create!(name: 'Hair Care')
+
+      discount_1 = merchant.bulk_discounts.create!(discount: 0.20, quantity: 10)
+      discount_2 = merchant.bulk_discounts.create!(discount: 0.30, quantity: 15)
+
+    visit "merchant/#{merchant.id}/bulk_discounts"
+    expect(page).to have_link('Create Discount')
+
+    click_link('Create Discount')
+
+  end
+
+  it 'displays link to create a new discount' do
+      merchant = Merchant.create!(name: 'Hair Care')
+
+      discount_1 = merchant.bulk_discounts.create!(discount: 0.20, quantity: 10)
+      discount_2 = merchant.bulk_discounts.create!(discount: 0.30, quantity: 15)
+
+    visit "merchant/#{merchant.id}/bulk_discounts"
+
+    click_link('Create Discount')
+    expect(page).to have_current_path("/merchant/#{merchant.id}/bulk_discounts/new")
+  
+    fill_in('Discount', with: '0.30')
+    fill_in('Quantity', with: '10')
+    click_on('commit')
+
+    expect(page).to have_current_path("/merchant/#{merchant.id}/bulk_discounts")
+    expect(page).to have_content('0.3')
+    expect(page).to have_content('10')
   end
 
 end
