1e8c374e23c81d2bc8ed2f28fa4c29643afafcac
Oscar Santos <oscar303data@gmail.com>
2022-03-05 09:12:27 -0700

    Create test for delete discount

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe 'Bulk discounts Index Page' do
 
     click_link('Create Discount')
     expect(page).to have_current_path("/merchant/#{merchant.id}/bulk_discounts/new")
-  
+
     fill_in('Discount', with: '0.30')
     fill_in('Quantity', with: '10')
     click_on('commit')
@@ -66,4 +66,25 @@ RSpec.describe 'Bulk discounts Index Page' do
     expect(page).to have_content('10')
   end
 
-end
+  it 'can delete a discount' do
+      merchant = Merchant.create!(name: 'Hair Care')
+
+      discount_1 = merchant.bulk_discounts.create!(discount: 0.20, quantity: 10)
+      discount_2 = merchant.bulk_discounts.create!(discount: 0.30, quantity: 15)
+
+      visit "merchant/#{merchant.id}/bulk_discounts"
+
+     expect(page).to have_content(discount_1.discount)
+     within("discount-")
+
+     expect(page).to have_content(discount_1.discount)
+     expect(page).to have_content(discount_1.quantity)
+     expect(page).to have_link("Delete #{discount_1.discount}")
+
+     click_link("Delete #{discount_1.discount}")
+     expect(current_path).to eq("/merchant/#{merchant.id}/bulk_discounts")
+
+     expect(page).to_not have_content(discount_1.discount)
+     expect(page).to_not have_content(discount_1.quantity)
+   end
+  end
