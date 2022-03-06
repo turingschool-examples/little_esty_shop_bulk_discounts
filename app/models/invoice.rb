41f3c79ee9e13455e1e95546979890b57899ad0b
Oscar Santos <oscar303data@gmail.com>
2022-03-06 06:45:44 -0700

    create model method for applied discount

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -13,4 +13,16 @@ class Invoice < ApplicationRecord
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+  def self.applied_discount(invoice_item)
+    applied_discount = 1
+    BulkDiscount.order(discount: :asc).each do |discount|
+
+      if invoice_item.quantity >= discount.quantity
+        applied_discount = discount.discount
+      end
+    end
+    ((1 - applied_discount) * (invoice_item.unit_price * invoice_item.quantity)).round(2)
+
+  end
 end
