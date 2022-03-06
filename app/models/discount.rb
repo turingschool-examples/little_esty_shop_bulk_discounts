d32fb10acd4b2906d7bf6da3595432435b27ae45
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-06 16:00:55 -0700

    Model: Feat and spec for #format discount model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -7,4 +7,7 @@ class Discount < ApplicationRecord
   validates :percent_discount, presence: true, numericality: {only_integer: true}
   validates :threshold, presence: true, numericality: {only_integer: true}
 
+  def format_discount
+    percent_discount.to_s+ "%"
+  end
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe Discount, type: :model do
     it { should validate_presence_of(:threshold)}
     it { should validate_numericality_of(:threshold)}
   end
+
+  describe '#format_discount' do
+    it 'presents a discount in a human-readable manor' do
+      discount_1 = create(:discount)
+      discount_2 = create(:discount, percent_discount: 35)
+
+
+      expect(discount_1.format_discount).to eq("20%")
+      expect(discount_2.format_discount).to eq("35%")
+    end
+  end
 end
