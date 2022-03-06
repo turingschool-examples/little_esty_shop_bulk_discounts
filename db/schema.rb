8cbf597e5750d80cd60d0feb6f83a09ef4173ec1
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-06 08:00:10 -0700

    DB: Migration to add name to discount

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_181736) do
+ActiveRecord::Schema.define(version: 2022_03_06_143425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_03_05_181736) do
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["merchant_id"], name: "index_discounts_on_merchant_id"
   end
 
