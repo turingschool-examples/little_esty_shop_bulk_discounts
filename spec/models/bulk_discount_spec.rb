a89cfa21bf9d421eb76f8c487a5678827dca3186
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-06 16:26:25 -0700

    update spec tests

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Customer, type: :model do
+RSpec.describe BulkDiscount, type: :model do
   describe "validations" do
     it { should validate_presence_of :percentage }
     it { should validate_presence_of :quantity_threshold }
