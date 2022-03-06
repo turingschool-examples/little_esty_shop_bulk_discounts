38bf0e16452691248189613a03af00d78b269dbd
Oscar Santos <oscar303data@gmail.com>
2022-03-06 06:25:52 -0700

    Create test for discounts index page

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -87,4 +87,28 @@ RSpec.describe 'Bulk discounts Index Page' do
      expect(page).to_not have_content(discount_1.discount)
      expect(page).to_not have_content(discount_1.quantity)
    end
+
+   #USER STORY 2
+   
+   it "displays next 3 upcoming holidays" do
+     merchant = Merchant.create!(name: 'Hair Care')
+
+     #discount_1 = merchant.bulk_discounts.create!(discount: 0.20, quantity: 10)
+
+     holiday_1 = Holiday.new({name: "Good Friday", date: "2022-04-15"})
+     holiday_2 = Holiday.new({name: "Memorial Day", date: "2022-05-30"})
+     holiday_3 = Holiday.new({name: "Juneteenth", date: "2022-06-20"})
+
+     visit "merchant/#{merchant.id}/bulk_discounts"
+
+     expect(page).to have_content("Upcoming Holidays")
+
+     expect(page).to have_content(holiday_1.name)
+     expect(page).to have_content(holiday_1.date)
+     expect(page).to have_content(holiday_2.name)
+     expect(page).to have_content(holiday_2.date)
+     expect(page).to have_content(holiday_3.name)
+     expect(page).to have_content(holiday_3.date)
+
+   end
   end
