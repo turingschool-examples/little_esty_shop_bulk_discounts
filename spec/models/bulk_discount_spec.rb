fa95b2fce89bde5d1f785efbffa046802dab0f7a
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-06 20:12:48 -0700

    update merchant model, create bulk_discount model, fix tests

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
   describe "validations" do
-    it { should validate_presence_of :percentage }
-    it { should validate_presence_of :quantity_threshold }
+    it { should validate_presence_of :percent }
+    it { should validate_presence_of :threshold }
     it { should validate_presence_of :merchant_id }
   end
 
