b333a332111888edbf966b731b3b78c60e30561a
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-06 21:55:46 -0700

    update BulkDiscounts#show, create show page

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -2,4 +2,9 @@ class BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = BulkDiscount.find(params[:id])
+  end
 end
