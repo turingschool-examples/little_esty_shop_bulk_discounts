5a2b4cb428ade6c57e134ca8c2bfe4340aedee7e
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-06 23:45:26 -0700

    Create #new

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -8,4 +8,8 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
 end
