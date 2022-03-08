e36c2176fca2601efbffda707724e26d3e8b6963
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-03-08 11:08:26 -0700

    Model tests passing - 100% coverage

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -14,6 +14,18 @@ class Invoice < ApplicationRecord
     invoice_items.sum("unit_price * quantity")
   end
 
+  def discount
+    invoice_items.joins(:bulk_discounts)
+      .select("invoice_items.id, max(invoice_items.unit_price * invoice_items.quantity * (bulk_discounts.percent_discount / 100.00)) as total_discount")
+      .where("invoice_items.quantity >= bulk_discounts.qty_threshold")
+      .group("invoice_items.id")
+      .sum(&:"total_discount")
+  end
+
+  def discounted_total_revenue
+    total_revenue - discount
+  end
+
   def items_by_merchant(merch_id)
     items.where('merchant_id = ?', merch_id)
   end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -14,36 +14,65 @@ RSpec.describe Invoice, type: :model do
 
   describe "instance methods" do
     before :each do
-      @merchant1 = Merchant.create!(name: 'Hair Care')
-      @merchant2 = Merchant.create!(name: 'Jewelry')
+      @merchant_1 = Merchant.create!(name: 'Mugs Galore', status: 0)
+      @merchant_2 = Merchant.create!(name: 'Happy Candles', status: 0)
 
-      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
-      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant2.id)
-      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
-      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
-      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 20, unit_price: 10, status: 2)
-      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+      @item_1 = @merchant_1.items.create!(name: "Coffee Mug", description: "fancy fancy", unit_price: 100, status: 0)
+      @item_2 = @merchant_1.items.create!(name: "Big Coffee Mug", description: "the big one", unit_price: 200, status: 0)
+      @item_3 = @merchant_2.items.create!(name: "Candles", description: "These go through your ears", unit_price: 50, status: 1)
 
-      @discount1 = BulkDiscount.create!(percent_discount: 20, qty_threshold:20,merchant_id: @merchant1.id)
+      @customer_1 = Customer.create!(first_name: "Harry", last_name: "Potter")
+
+      @invoice_1 = @customer_1.invoices.create!(status: 1, created_at: '2012-03-25 09:54:09')
+
+      @invoice_item_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, unit_price: @item_1.unit_price, quantity: 10, status: 0)
+      @invoice_item_2 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_1.id, unit_price: @item_2.unit_price, quantity: 5, status: 0)
+      @invoice_item_3 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_1.id, unit_price: @item_3.unit_price, quantity: 10, status: 0)
+
+      @bulk_discount_1 = @merchant_1.bulk_discounts.create!( qty_threshold: 10, percent_discount: 10)
+      @bulk_discount_2 = @merchant_2.bulk_discounts.create!( qty_threshold: 5, percent_discount: 5)
     end
 
-    it "total_revenue" do
-      expect(@invoice_1.total_revenue).to eq(210)
+    it '#total_revenue' do
+      total_revenue = (@invoice_item_1.unit_price * @invoice_item_1.quantity) + (@invoice_item_2.unit_price * @invoice_item_2.quantity) + (@invoice_item_3.unit_price * @invoice_item_3.quantity)
+
+      expect(@invoice_1.total_revenue).to eq(total_revenue)
     end
 
-    it "total_revenue_by_merchant" do
+    it '#discounted_total_revenue' do
+      total_revenue = @invoice_1.total_revenue - @invoice_1.discount
 
-      expect(@invoice_1.total_revenue_by_merchant(@merchant1.id)).to eq(200)
+      expect(@invoice_1.discounted_total_revenue).to eq(total_revenue)
     end
 
-    it "discounts_by_merchant" do
+    it '#total_revenue_by_merchant unit price by quantity of item on invoice and adds together to show revenue of merchant' do
+      total_revenue = (@invoice_item_1.unit_price * @invoice_item_1.quantity) + (@invoice_item_2.unit_price * @invoice_item_2.quantity)
 
-      expect(@invoice_1.discounts_by_merchant(@merchant1.id)).to eq(40)
+      expect(@invoice_1.total_revenue_by_merchant(@merchant_1.id)).to eq(total_revenue)
     end
 
-    it "discounted_revenue_by_merchant" do
+    it '#discounted_revenue_by_merchant(merch_id)' do
+      total_revenue = (90) * @invoice_item_1.quantity + (@invoice_item_2.unit_price * @invoice_item_2.quantity)
 
-      expect(@invoice_1.discounted_revenue_by_merchant(@merchant1.id)).to eq(160)
+      expect(@invoice_1.discounted_revenue_by_merchant(@merchant_1.id)).to eq(total_revenue)
+    end
+
+    it '#discounts_by_merchant - revenue of merchant on invoice with bulk discounts applied' do
+      total_discount = @invoice_item_1.unit_price * @invoice_item_1.quantity * (@bulk_discount_1.percent_discount / 100.00)
+
+      expect(@invoice_1.discounts_by_merchant(@merchant_1.id)).to eq(total_discount)
+    end
+
+    it "other items from other merchants not affected" do
+      total_discount = @invoice_item_1.unit_price * @invoice_item_1.quantity * (@bulk_discount_1.percent_discount / 100.00)
+
+      expect(@invoice_1.discounts_by_merchant(@merchant_1.id)).to eq(total_discount)
+    end
+
+    it '#discount - amount discounted from invoice' do
+      total_discounts_invoice_1 = (@invoice_item_2.unit_price * @invoice_item_2.quantity * (@bulk_discount_1.percent_discount / 100.00)) + (@invoice_item_3.unit_price * @invoice_item_3.quantity * (@bulk_discount_2.percent_discount / 100.00)).to_f
+
+      expect(@invoice_1.discount).to eq(total_discounts_invoice_1.to_f)
     end
   end
 end
