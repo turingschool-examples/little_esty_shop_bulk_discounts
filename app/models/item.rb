8070fedc4d2716e640229939a09c83946d9530f7
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-09 03:21:06 -0700

    update item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,10 @@ class Item < ApplicationRecord
 
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  
   belongs_to :merchant
+  has_many :bulk_discounts, through: :merchant
+
 
   enum status: [:disabled, :enabled]
 
