d5baf6d2b8a1f7bade4d51af11bfe61a0de0921a
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-09 03:24:39 -0700

    update InvoiceItem model test

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe InvoiceItem, type: :model do
   describe "relationships" do
     it { should belong_to :invoice }
     it { should belong_to :item }
+    it { should have_many(:bulk_discounts).through(:item) }
   end
 
   describe "class methods" do
