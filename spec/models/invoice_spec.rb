8704a5a7df4db93cdad334ac70a55ba9211d07d4
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-09 03:06:07 -0700

    Create ::discounted_revenue model test

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -14,14 +14,37 @@ RSpec.describe Invoice, type: :model do
   describe "instance methods" do
     it "total_revenue" do
       @merchant1 = Merchant.create!(name: 'Hair Care')
+
       @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
       @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+
       @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
       @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+
       @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
       @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
 
       expect(@invoice_1.total_revenue).to eq(100)
+    end
+
+    it '::discounted_revenue' do
+      @merchant1 = Merchant.create!(name: 'Hair Care')
+
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
+      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+      
+      @transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_1.id)
+
+      @bulk_1 = @merchant1.bulk_discounts.create!(percent: 10, threshold: 10)
+      @bulk_2 = @merchant1.bulk_discounts.create!(percent: 15, threshold: 15)
+
+      expect(@invoice_1.discounted_revenue).to eq(10.0)
     end
   end
 end
