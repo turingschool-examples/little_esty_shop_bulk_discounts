c82298764414835eb5add5a4bfe8289b12a87c4e
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-09 03:50:15 -0700

    create #discount_applied?

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -16,4 +16,11 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  def discount_applied?
+    discount = bulk_discounts
+    .where("? >= threshold", quantity)
+    .order(percent: :desc)
+    .first
+  end
 end
