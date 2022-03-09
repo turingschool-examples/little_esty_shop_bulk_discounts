0513e9050cf1411f44e124f1c9c960a1dca544bf
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-09 03:46:10 -0700

    craete discount applied display test

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -91,6 +91,29 @@ RSpec.describe 'invoices show' do
     expect(page).to have_content(@invoice_1.discounted_revenue)
   end
 
+  it 'displays discount for specific item if applied' do
+    @merchant1 = Merchant.create!(name: 'Hair Care')
+
+    @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+    @item_2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: @merchant1.id)
+
+    @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+    @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+
+    @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 2)
+    @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 5, unit_price: 10, status: 2)
+
+    @transaction1 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_1.id)
+
+    @bulk_1 = @merchant1.bulk_discounts.create!(percent: 10, threshold: 10)
+    @bulk_2 = @merchant1.bulk_discounts.create!(percent: 15, threshold: 15)
+
+    visit merchant_invoice_path(@merchant1, @invoice_1)
+
+    expect(page).to have_content("Discount Applied: #{@bulk_1.id}")
+    expect(page).to have_link("#{@bulk_1.id}")
+  end
+
   it "shows a select field to update the invoice status" do
     visit merchant_invoice_path(@merchant1, @invoice_1)
 
