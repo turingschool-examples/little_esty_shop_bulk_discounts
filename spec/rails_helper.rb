b5b531cd9a65f3e5389a07e68260921ae9c35118
John Napier <jcee.napier@gmail.com>
2022-03-21 21:03:40 -0600

    Add vcr and webmock gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,11 +63,20 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  Shoulda::Matchers.configure do |config|
-      config.integrate do |with|
-        with.test_framework :rspec
-        with.library :rails
-      end
-    end
+  config.include Capybara::DSL
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+end
+
+
