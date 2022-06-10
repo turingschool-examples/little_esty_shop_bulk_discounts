93546fdee25e850b00fb74e8088a6228f2bcf369
Nick Jacobs <nickjacobsatc@gmail.com>
2022-06-10 17:05:01 -0500

    Feat: added new and create controller actions

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -7,5 +7,21 @@ class DiscountsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @discount = Discount.find(params[:id])
-  end 
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    discount = @merchant.discounts.create!(discount_params)
+    redirect_to merchant_discounts_path(@merchant.id)
+  end
+
+  private
+  def discount_params
+    params.permit(:name,:threshold,:percentage)
+  end
+
 end
