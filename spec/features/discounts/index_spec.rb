0ba811176d81162869b3f50338435d3a896255ee
Nick Jacobs <nickjacobsatc@gmail.com>
2022-06-10 17:25:37 -0500

    Refactor: added visit path to before each

--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -6,14 +6,13 @@ RSpec.describe "Discounts Index Page" do
     @merchant2 = Merchant.create!(name: 'Test Merchant')
     @discount1 = @merchant1.discounts.create!(threshold: 5, percentage: 5, name: "5% Discount")
     @discount2 = @merchant1.discounts.create!(threshold: 15, percentage: 15, name: "15% Discount")
-
     @discount3 = @merchant2.discounts.create!(threshold: 10, percentage: 10, name: "10% Discount")
+
+    visit merchant_discounts_path(@merchant1)
   end
 
 
   it "shows all discounts offered by this merchant" do
-    visit merchant_discounts_path(@merchant1)
-
     expect(page).to have_content("5% Discount")
     expect(page).to have_content("15% Discount")
     expect(page).to have_content("Current Discounts for Hair Care:")
@@ -28,9 +27,7 @@ RSpec.describe "Discounts Index Page" do
   end
 
   it "has a link to create a new discount" do
-    visit merchant_discounts_path(@merchant1)
     click_on "Create New Discount"
     expect(current_path).to eq(new_merchant_discount_path(@merchant1))
   end
-
 end
