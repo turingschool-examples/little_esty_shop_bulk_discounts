18b8a0ce0d3ca8c3abdd7c0e2ee827474df98f3e
Nick Jacobs <nickjacobsatc@gmail.com>
2022-06-10 17:24:47 -0500

    Feat: added destroy controller action

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -19,6 +19,13 @@ class DiscountsController < ApplicationController
     redirect_to merchant_discounts_path(@merchant.id)
   end
 
+  def destroy
+    @merchant = Merchant.find(params[:merchant_id])
+    discount = Discount.find(params[:id])
+    discount.destroy 
+    redirect_to merchant_discounts_path(@merchant.id)
+  end
+
   private
   def discount_params
     params.permit(:name,:threshold,:percentage)
