07c1708a1ae0aadd097c314e00747501a9dc28aa
Nick Jacobs <nickjacobsatc@gmail.com>
2022-06-11 12:51:28 -0500

    Feat: added find_discount, pre_discount_revenue, and discounted_revenue methods

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,11 +7,32 @@ class InvoiceItem < ApplicationRecord
 
   belongs_to :invoice
   belongs_to :item
+  has_one :merchant
+  has_many :discounts, through: :merchant
 
   enum status: [:pending, :packaged, :shipped]
 
   def self.incomplete_invoices
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
+  end
+
+  def find_discount
+    item.merchant.discounts.where('discounts.threshold <= ?', quantity)
+                           .select('discounts.*')
+                           .order(percentage: :desc)
+                           .first
+  end
+
+  def pre_discount_revenue
+    unit_price * quantity
+  end
+
+  def discounted_revenue
+    if !find_discount.present?
+      pre_discount_revenue
+    else
+      pre_discount_revenue - (pre_discount_revenue * (find_discount.percentage.to_f * 0.01))
+    end
   end
 end
