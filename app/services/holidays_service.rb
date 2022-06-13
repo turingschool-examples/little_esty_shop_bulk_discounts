e795f469099389b493f74af6317816a898c7b1b3
Nick Jacobs <nickjacobsatc@gmail.com>
2022-06-13 15:41:10 -0500

    Refactor: moved holidays method to facades

--- app/services/holidays_service.rb
+++ app/services/holidays_service.rb
@@ -4,10 +4,4 @@ class HolidaysService
     connection = Faraday.get("https://date.nager.at/api/v3/NextPublicHolidays/US")
     parsed = JSON.parse(connection.body, symbolize_names: true )
   end
-
-  def self.holidays
-    get_data.map do |data|
-      Holiday.new(data)
-    end
-  end
 end
