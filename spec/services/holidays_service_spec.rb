654d1e4b39ea79f4a3b5fa8aa95777b4be108b7b
Nick Jacobs <nickjacobsatc@gmail.com>
2022-06-13 15:42:13 -0500

    Test: service exists

--- spec/services/holidays_service_spec.rb
+++ spec/services/holidays_service_spec.rb
@@ -2,18 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Holiday API service" do
 
-  it 'can get next holiday names from API' do
-    service = HolidaysService.holidays[0..2]
-    expect(service[0].name).to eq("Juneteenth")
-    expect(service[1].name).to eq("Independence Day")
-    expect(service[2].name).to eq("Labour Day")
+  it "exists" do
+    service = HolidaysService.new
+    expect(service).to be_a(HolidaysService)
   end
-
-  it "can give the next three holiday's dates" do
-    service = HolidaysService.holidays[0..2]
-    expect(service[0].date).to eq("2022-06-20")
-    expect(service[1].date).to eq("2022-07-04")
-    expect(service[2].date).to eq("2022-09-05")
-  end
-
 end
