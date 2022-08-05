968784b8088994d994b1ee08e7bd268f3084cbe4
Nicole Esquer <nicole.esquer5@gmail.com>
2022-08-04 20:29:04 -0700

    add discounts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_204649) do
+ActiveRecord::Schema.define(version: 2022_08_05_001918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2021_03_04_204649) do
     t.string "city"
     t.string "state"
     t.bigint "zip"
+  end
+
+  create_table "discounts", force: :cascade do |t|
+    t.integer "percentage"
+    t.integer "quantity"
+    t.bigint "merchant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["merchant_id"], name: "index_discounts_on_merchant_id"
   end
 
   create_table "invoice_items", force: :cascade do |t|
@@ -74,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_03_04_204649) do
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
+  add_foreign_key "discounts", "merchants"
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
