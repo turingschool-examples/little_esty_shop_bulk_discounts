6c8adb080307ea4df99206f87cdf5c4e9b382e70
Bryan Shears <brshears@gmail.com>
2022-08-06 15:52:40 -0600

    Add feature test for discount show page

---
+++ spec/features/dashboard/bulk_discounts/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant bulk discounts show page' do
+
+  before :each do
+    @merchant1 = Merchant.create!(name: 'Hair Care')
+    @merchant2 = Merchant.create!(name: 'Po House Pub')
+
+    @discount_1 = BulkDiscount.create!(percentage_discount: 5, quantity:  10, merchant_id: @merchant1.id)
+    @discount_2 = BulkDiscount.create!(percentage_discount: 78, quantity:  68, merchant_id: @merchant2.id)
+
+    visit merchant_bulk_discount_path(@merchant1, @discount_1)
+  end
+
+  it 'displays the bulk discounts quantity threshold and percentage discount' do
+
+      expect(page).to have_content('Percentage Discount: 5')
+      expect(page).to have_content('Quantity: 10')
+    
+      expect(page).to_not have_content('Percentage Discount: 78')
+      expect(page).to_not have_content('Quantity: 68')
+  end
+
+end
