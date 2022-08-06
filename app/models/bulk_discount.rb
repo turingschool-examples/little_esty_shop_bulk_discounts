d11a5e482d282fa50a5568a92be1db5bcb7734e0
Bryan Shears <brshears@gmail.com>
2022-08-06 15:53:45 -0600

    Update views and controller  for discount show page

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,8 +3,4 @@ class BulkDiscount < ApplicationRecord
 
   validates :percentage_discount, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 99 }
   validates :quantity, presence: true, numericality: true
-
-  def bulk_discounts
-    merchant = Merchant.find(params[:merchant_id])
-  end
 end
