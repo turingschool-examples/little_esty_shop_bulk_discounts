e781293b2c07470daeb4e7f33f08d6710f1b8654
Bryan Shears <brshears@gmail.com>
2022-08-09 16:47:58 -0600

    Adds feature tests for link to applied discounts

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -30,13 +30,21 @@ RSpec.describe InvoiceItem, type: :model do
       @i3 = Invoice.create!(customer_id: @c2.id, status: 2)
       @i4 = Invoice.create!(customer_id: @c3.id, status: 2)
       @i5 = Invoice.create!(customer_id: @c4.id, status: 2)
-      @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 0)
+      @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 0)
       @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 1, unit_price: 8, status: 0)
       @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
       @ii_4 = InvoiceItem.create!(invoice_id: @i3.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 1)
     end
+
     it 'incomplete_invoices' do
       expect(InvoiceItem.incomplete_invoices).to eq([@i1, @i3])
+    end
+
+    it 'can find if their is a discount' do
+      discount_a = BulkDiscount.create!(percentage_discount: 20, quantity:  10, merchant_id: @m1.id)
+
+      expect(@ii_1.find_discount).to eq(discount_a)
+      expect(@ii_2.find_discount).to_not eq(discount_a)
     end
   end
 end
