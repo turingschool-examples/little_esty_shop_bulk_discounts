cdadafdba691ee4c196f0c601122b5271672f98f
Tyler Caudill <teecaud@hotmail.com>
2022-08-09 17:09:16 -0500

    refactor invoice index action

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -3,7 +3,7 @@ class InvoicesController < ApplicationController
   before_action :find_merchant, only: [:index]
 
   def index
-    @invoices = @merchant.invoices
+    @invoices = @merchant.invoices.distinct
   end
 
   def show
