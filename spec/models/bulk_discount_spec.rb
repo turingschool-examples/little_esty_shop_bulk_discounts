0d6f25dcad6a6361c073af0b41c95dd0acaae7f7
Lucas Colwell <colwellc37@gmail.com>
2022-09-24 12:58:58 -0600

    Add model test for bulk discounts

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should belong_to :merchant }
+    it { should have_many(:items).through(:merchant) }
+    it { should have_many(:invoice_items).through(:items) }
+    it { should have_many(:invoices).through(:invoice_items) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :percent_off}
+    it { should validate_presence_of :quantity}
+    it { should validate_numericality_of :quantity}
+    it { should validate_presence_of :merchant_id}
+    it { should define_enum_for(:status).with_values([:disabled, :enabled])}
+  end
+
+  pending 'methods' do
+
+  end
 end
