3087c153d0143dd6bdb1ac7c0f9a818efc7fcdfe
Lucas Colwell <colwellc37@gmail.com>
2022-09-24 17:48:29 -0600

    Test: story 3 BD show view

---
+++ spec/features/bulk_discounts/new_spec.rb
@@ -0,0 +1,25 @@
+# require 'rails_helper'
+#
+# RSpec.describe "As a merchant" do
+#   describe 'When I visit my bulk discounts index' do
+#     it 'Then I see a link to create a new discount' do
+#       visit merchant
+#     end
+#
+#     describe 'When I click this link' do
+#       it 'Then I am taken to a new page where I see a form to add a new bulk discount' do
+#
+#       end
+#
+#       describe 'When I fill in the form with valid data' do
+#         it 'Then I am redirected back to the bulk discount index' do
+#
+#         end
+#
+#         it 'And I see my new bulk discount listed' do
+#
+#         end
+#       end
+#     end
+#   end
+# end
