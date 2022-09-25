e811d35b9b4d0888146ab82d0331368dce1990bc
Lucas Colwell <colwellc37@gmail.com>
2022-09-25 13:29:13 -0600

    Add edit to BD controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -15,10 +15,26 @@ class BulkDiscountsController < ApplicationController
   def create
     bulk_discount = @merchant.bulk_discounts.create(bulk_discount_params)
     if bulk_discount.save
+      flash.clear #sad test needed
       redirect_to [@merchant, :bulk_discounts]
     else
       flash.notice = "error"
       render :new
+    end
+  end
+
+  def edit
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def update
+    discount = BulkDiscount.find(params[:id])
+    discount.update(bulk_discount_params)
+    if discount.save
+      redirect_to [@merchant, discount]
+    else
+      # flash.notice = "All fields must be filled"
+      redirect_to edit_merchant_item_path(@merchant, discount)
     end
   end
 
