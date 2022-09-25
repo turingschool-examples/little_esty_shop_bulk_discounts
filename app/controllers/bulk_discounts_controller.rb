41a5698505c64eb33032aa75569b618d14056d83
Lucas Colwell <colwellc37@gmail.com>
2022-09-24 21:53:25 -0600

    Add strong params and create

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -12,6 +12,22 @@ class BulkDiscountsController < ApplicationController
   def new
   end
 
+  def create
+    bulk_discount = @merchant.bulk_discounts.create(bulk_discount_params)
+    if bulk_discount.save
+      redirect_to [@merchant, :bulk_discounts]
+    else
+      flash.notice = "error"
+      render :new
+    end
+  end
+
+  private
+
+  def bulk_discount_params
+    params.permit(:percent_off, :quantity, :status)
+  end
+
   def find_merchant
     @merchant = Merchant.find(params[:merchant_id])
   end
