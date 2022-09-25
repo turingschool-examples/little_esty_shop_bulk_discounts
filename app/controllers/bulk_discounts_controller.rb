b605c6947ddb111598a7b443468ee6fc64ef1c3b
Lucas Colwell <colwellc37@gmail.com>
2022-09-24 18:47:28 -0600

    Add to BD controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,6 +1,18 @@
 class BulkDiscountsController < ApplicationController
+  before_action :find_merchant
+
   def index
-    @merchant = Merchant.find(params[:merchant_id])
     @bulk_discounts = BulkDiscount.all
+  end
+
+  def show
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def new
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
