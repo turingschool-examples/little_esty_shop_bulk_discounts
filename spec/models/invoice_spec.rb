f66bda4e9d89b7f465b856cc88c0548f5f508a75
Lucas Colwell <colwellc37@gmail.com>
2022-09-26 15:10:19 -0600

    Test: Fix naming and interpolation

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe Invoice, type: :model do
       expect(@invoice_1.total_revenue).to eq(100)
     end
 
-    it "discounted_revenue" do
+    it "total_discount" do
       @merchant1 = Merchant.create!(name: 'Hair Care')
       @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
       @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
