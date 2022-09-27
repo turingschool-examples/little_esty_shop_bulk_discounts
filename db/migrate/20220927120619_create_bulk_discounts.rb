6958f05c0b2fc0f0602af015d0bc85c09d68084d
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-27 07:26:21 -0600

    Set up tests and migrations for bulk discounts

---
+++ db/migrate/20220927120619_create_bulk_discounts.rb
@@ -0,0 +1,11 @@
+class CreateBulkDiscounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bulk_discounts do |t|
+      t.integer :threshold
+      t.float :discount_percent
+      t.references :merchant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
