1a2356ce6f9e5ae2729c58990b32e06d897c8204
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-27 09:13:30 -0600

    Add tests for create action and form

--- spec/features/bulk_discounts/new_spec.rb
+++ spec/features/bulk_discounts/new_spec.rb
@@ -2,19 +2,31 @@ require 'rails_helper'
 
 RSpec.describe 'Bulk Discount creation form: as a merchant' do
   describe "when I visit the page to create a new bulk discount " do
+
     before(:each) do
-      visit new_merchant_bulk_discount_path
+      @merchant1 = Merchant.create!(name: 'Hair Care')
+      visit new_merchant_bulk_discount_path(@merchant1)
     end
+
     it "has a form to create a new discount" do
       expect(page).to have_field(:threshold)
       expect(page).to have_field(:discount_percent)
     end
 
     describe "when I fill out that form with the required information, and click submit" do
+      before(:each) do
+        fill_in "discount_percent", with: "50"
+        fill_in "threshold", with: "100"
+        click_on "Create New Discount"
+      end
+
       it "I am redirected to the index page" do
+        expect(current_path).to eq merchant_bulk_discounts_path(@merchant1)
       end
 
       it "And I see the new bulk discount" do
+        expect(page).to have_content(100)
+        expect(page).to have_content(50)
       end
     end
 
@@ -24,6 +36,6 @@ RSpec.describe 'Bulk Discount creation form: as a merchant' do
 
       it "And I see a message telling me of an error" do
       end
-    end  
+    end
   end
 end
