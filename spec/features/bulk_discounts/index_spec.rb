82e1944b787d5a3665eb9c23f82b73bb9eb219a6
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-27 08:42:05 -0600

    Add tests for navigating to show page and new form page

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -40,19 +40,44 @@ RSpec.describe 'merchant dashboard' do
     @transaction6 = Transaction.create!(credit_card_number: 879799, result: 1, invoice_id: @invoice_7.id)
     @transaction7 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_2.id)
 
-    @bulk_discount1 = BulkDiscount.create!(merchant_id: merchant1.id, threshold: 5, discount_percent: 10)
-    @bulk_discount2 = BulkDiscount.create!(merchant_id: merchant1.id, threshold: 15, discount_percent: 20)
+    @bulk_discount1 = BulkDiscount.create!(merchant_id: @merchant1.id, threshold: 5, discount_percent: 10)
+    @bulk_discount2 = BulkDiscount.create!(merchant_id: @merchant1.id, threshold: 15, discount_percent: 20)
+    visit merchant_bulk_discounts_path(@merchant1)
   end
 
 #   Where I see all of my bulk discounts including their
 # percentage discount and quantity thresholds
 # And each bulk discount listed includes a link to its show page
-  describe "as a Merchant, when I visit the page"
+  describe "as a Merchant, when I visit the page" do
     it "I see all of my bulk discounts including discount% and quantity thresholds" do
-      expect(page).to have_content(bulk_discount1.id)
-      expect(page).to have_content(bulk_discount2.id)
-      expect(page).to have_content(bulk_discount2.threshold)
-      expect(page).to have_content(bulk_discount1.discount_percent)
+      expect(page).to have_content(@bulk_discount1.id)
+      expect(page).to have_content(@bulk_discount2.id)
+      expect(page).to have_content(@bulk_discount2.threshold)
+      expect(page).to have_content(@bulk_discount1.discount_percent)
+    end
+
+    it "And each bulk discount listed includes a link to that discounts show page" do
+      within "#discount-#{@bulk_discount1.id}" do
+        expect(page).to have_link('View this Discount')
+      end
+      within "#discount-#{@bulk_discount1.id}" do
+        expect(page).to have_link('View this Discount')
+      end
+    end
+
+    it "when I click the link, I am taken to the show page for that discount" do
+      within "#discount-#{@bulk_discount1.id}" do
+        click_link "View this Discount"
+        expect(current_path).to eq merchant_bulk_discount_path(@merchant1, @bulk_discount1)
+      end
+    end
+  end
+
+  describe "creating a new discount" do
+    it "i see a button to create a new discount" do
+      expect(page).to have_link("Create New Bulk Discount")
+      click_link("Create New Bulk Discount")
+      expect(current_path).to eq new_merchant_bulk_discount_path(@merchant1)
     end
   end
 end
