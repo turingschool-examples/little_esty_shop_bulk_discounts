59bf555ea589377881a1549c790cd8b6f9a17d38
Kevin Ta <kvnta@yahoo.com>
2022-11-16 01:32:59 -0700

    Change enum status

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -9,7 +9,7 @@ class Invoice < ApplicationRecord
   has_many :merchants, through: :items
   has_many :bulk_discounts, through: :merchants
 
-  enum status: [:cancelled, 'in progress', :complete]
+  enum status: [:cancelled, 'in progress', :completed]
 
   def total_revenue
     invoice_items.sum("unit_price * quantity")
