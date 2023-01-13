44a8feb750d4432407b27d27cb2eca42c89effc5
Braxton Eddings <beddings81@gmail.com>
2023-01-12 23:16:12 -0600

    Add test for a link to dicscounts in merchant dashboard

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -119,4 +119,12 @@ RSpec.describe 'merchant dashboard' do
   it "shows the date that the invoice was created in this format: Monday, July 18, 2019" do
     expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %-d, %Y"))
   end
+
+  it 'contains a link to its bulk discounts index page' do
+    expect(page).to have_link("My Discounts")
+
+    click_link "My Discounts"
+
+    expect(current_path).to eq(merchant_bulk_discounts_path(@merchant1))
+  end
 end
