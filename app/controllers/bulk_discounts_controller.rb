16e460b9c49a1d4685701d925ac422c34aac579c
Kyle Ledin <Kyle.Ledin85@gmail.com>
2023-01-13 17:43:08 -0600

    Has story 2 passing all test

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -2,4 +2,24 @@ class BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+  
+  def create
+    bulk_discount = BulkDiscount.new(bulk_discount_params)
+    @merchant = Merchant.find(params[:merchant_id])
+    if bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(@merchant.id)
+    else
+      redirect_to merchant_bulk_discounts_path(@merchant.id)
+    end
+  end
+
+  private
+
+  def bulk_discount_params
+    params.permit(:id, :merchant_id, :percentage_discount, :quantity_threshold)
+  end
 end
