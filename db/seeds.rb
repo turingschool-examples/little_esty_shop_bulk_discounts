cb5737285abf17a31447ae2dece0b4c3d5ba8db8
Kelsie Porter <krporter22@gmail.com>
2023-01-14 16:32:02 -0700

    Setup and seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,16 @@
-require 'faker'
+Discount.destroy_all
+
+merchant_1 = Merchant.find(1)
+merchant_2 = Merchant.find(2)
+
+merchant_1.discounts.create!(threshold: 5, percentage: 5)
+merchant_1.discounts.create!(threshold: 10, percentage: 10)
+merchant_1.discounts.create!(threshold: 25, percentage: 25)
+merchant_1.discounts.create!(threshold: 30, percentage: 30)
+merchant_1.discounts.create!(threshold: 50, percentage: 50)
+merchant_2.discounts.create!(threshold: 8, percentage: 10)
+merchant_2.discounts.create!(threshold: 6, percentage: 30)
+merchant_2.discounts.create!(threshold: 10, percentage: 20)
+merchant_2.discounts.create!(threshold: 26, percentage: 75)
+merchant_2.discounts.create!(threshold: 7, percentage: 15)
+merchant_2.discounts.create!(threshold: 3, percentage: 50)
