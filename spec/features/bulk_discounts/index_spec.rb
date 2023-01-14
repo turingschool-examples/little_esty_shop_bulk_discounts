d6cb7f03c6d2ba30802942fb39e73f580a6c1e9d
Alastair Lloyd <dex8056@aol.com>
2023-01-14 09:08:18 -0700

    add tests for user story1

---
+++ spec/features/bulk_discounts/index_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe "bulk discounts index" do
+  before(:each) do
+    @merchant1 = Merchant.create!(name: 'Hair Care')
+    @merchant2 = Merchant.create!(name: 'Jewelry')
+
+    @bd1 = @merchant1.bulk_discounts.create!(percentage: 25, quantity_threshold: 30)
+    @bd2 = @merchant1.bulk_discounts.create!(percentage: 35, quantity_threshold: 60)
+    @bd3 = @merchant2.bulk_discounts.create!(percentage: 22, quantity_threshold: 33)
+  end
+
+  it 'shows all bulk discounts, and their percentage and thresholds' do
+    visit merchant_bulk_discounts_path(@merchant1)
+
+    expect(page).to have_content("Discount No #{@bd1.id}")
+    expect(page).to have_content("Percentage Discount: #{@bd1.percentage}")
+    expect(page).to have_content("Quantity Threshold: #{@bd1.quantity_threshold}")
+    expect(page).to have_content("Discount No #{@bd2.id}")
+    expect(page).to have_content("Percentage Discount: #{@bd2.percentage}")
+    expect(page).to have_content("Quantity Threshold: #{@bd2.quantity_threshold}")
+
+    expect(page).to_not have_content("Discount No #{@bd3.id}")
+    expect(page).to_not have_content("Percentage Discount: #{@bd3.percentage}")
+    expect(page).to_not have_content("Quantity Threshold: #{@bd3.quantity_threshold}")
+  end
+
+  it 'for each discount id it is a link to the discount show page' do
+    visit merchant_bulk_discounts_path(@merchant1)
+
+    click_link "Discount No #{@bd1.id}"
+
+    expect(page).to have_current_path("/merchant/#{@merchant1.id}/bulk_discounts.#{@bd1.id}")
+  end
+end
--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -93,6 +93,7 @@ RSpec.describe 'merchant dashboard' do
     expect(page).to have_no_content(@customer_6.first_name)
     expect(page).to have_no_content(@customer_6.last_name)
   end
+
   it "can see a section for Items Ready to Ship with list of names of items ordered and ids" do
     within("#items_ready_to_ship") do
 
@@ -118,5 +119,13 @@ RSpec.describe 'merchant dashboard' do
 
   it "shows the date that the invoice was created in this format: Monday, July 18, 2019" do
     expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %-d, %Y"))
+  end
+
+  it 'has a link to view all my discounts' do
+    expect(page).to have_link("Discounts")
+
+    click_on "Discounts"
+
+    expect(current_path).to eq(merchant_bulk_discounts_path(@merchant1))
   end
 end
