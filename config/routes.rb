b7e2a4c995cede9852169789d5e8d7c78354d0d4
Alastair Lloyd <dex8056@aol.com>
2023-01-14 09:13:08 -0700

    amend routes to support bulk_discounts through merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchant, only: [:show] do
     resources :dashboard, only: [:index]
     resources :items, except: [:destroy]
     resources :item_status, only: [:update]
     resources :invoices, only: [:index, :show, :update]
+    resources :bulk_discounts, only: [:index]
   end
-
+  
   namespace :admin do
     resources :dashboard, only: [:index]
     resources :merchants, except: [:destroy]
