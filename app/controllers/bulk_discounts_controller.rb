a63ed229ff8e6b6e7f09071a339ffc9d5fbb5041
Anhnhi Tran <anhtran811@gmail.com>
2023-01-14 13:34:26 -0700

    Completes user story 2, also made correction to US 1 to add show page

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -2,4 +2,27 @@ class BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    
+  end
+  
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+  
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    BulkDiscount.create!(bulk_discounts_params)
+    # BulkDiscount.create!(quantity_threshold: params[:quantity_threshold],
+    #                     percentage: params[:percentage],
+    #                     merchant_id: params[:merchant_id])
+    redirect_to merchant_bulk_discounts_path(merchant)
+  end
+
+  private
+  
+  def bulk_discounts_params
+    params.permit(:quantity_threshold, :percentage, :merchant_id)
+  end
 end
--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -22,7 +22,21 @@ RSpec.describe "Create new bulk discount" do
       expect(page).to have_link('Create bulk discount')
     end
 
+    it 'has a form to add a new bulk discount' do
+      merchant_1 = create(:merchant)
 
-    it 'has a form to add a new bulk discount'
+      visit merchant_bulk_discounts_path(merchant_1)
+
+      click_link('Create bulk discount')
+
+      fill_in('quantity_threshold', with: 20)
+      fill_in('Percentage', with: 15)
+    
+      click_button('Create Bulk Discount')
+      
+      expect(current_path).to eq(merchant_bulk_discounts_path(merchant_1))
+      expect(page).to have_content("Quantity Threshold: 20")
+      expect(page).to have_content("Percentage: 15")
+    end
   end
 end
