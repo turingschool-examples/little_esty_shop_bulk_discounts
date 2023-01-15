9e2e20f45f30a4159945c91960468bd56b088b68
Anhnhi Tran <anhtran811@gmail.com>
2023-01-14 17:13:54 -0700

    feat/test for link to appear on show page

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -22,4 +22,25 @@ RSpec.describe 'Bulk Discounts Show Page' do
       expect(page).to_not have_content(bulk_discount_2.percentage)
     end
   end
+
+# As a merchant
+# When I visit my bulk discount show page
+# Then I see a link to edit the bulk discount
+# When I click this link
+# Then I am taken to a new page with a form to edit the discount
+# And I see that the discounts current attributes are pre-poluated in the form
+# When I change any/all of the information and click submit
+# Then I am redirected to the bulk discount's show page
+# And I see that the discount's attributes have been updated
+  describe 'User story 5 (part 1)' do
+    it 'has a link to the edit the bulk discount' do
+      merchant_1 = create(:merchant)
+
+      bulk_discount_1 = merchant_1.bulk_discounts.create!(quantity_threshold: 10, percentage: 5)
+
+      visit merchant_bulk_discount_path(merchant_1, bulk_discount_1.id)
+
+      expect(page).to have_link('Edit bulk discount')
+    end
+  end
 end
