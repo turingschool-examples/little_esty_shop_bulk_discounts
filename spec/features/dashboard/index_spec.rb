fae1bd856f561b44ddd5ba57cec2796bab700b23
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-15 00:19:44 -0600

    add merchant dashboard link to bulk discounts index

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -119,4 +119,11 @@ RSpec.describe 'merchant dashboard' do
   it "shows the date that the invoice was created in this format: Monday, July 18, 2019" do
     expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %-d, %Y"))
   end
+  
+  it 'has a link to the the bulk discounts index page' do
+    expect(page).to have_link("Bulk Discounts")
+
+    click_link("Bulk Discounts")
+    expect(current_path).to eq(merchant_bulk_discounts_path(@merchant1))
+  end
 end
