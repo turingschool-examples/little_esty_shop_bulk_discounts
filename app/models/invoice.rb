6d7bf145605f92a939e680b3d76fa2aabcfadf9c
Anhnhi Tran <anhtran811@gmail.com>
2023-01-15 12:33:40 -0700

    Adds bulk discount relationship

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,10 +7,15 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :bulk_discounts, through: :merchants
 
   enum status: [:cancelled, 'in progress', :completed]
 
   def total_revenue
     invoice_items.sum("unit_price * quantity")
+  end
+
+  def total_revenue_with_discount(merchant)
+    require 'pry'; binding.pry
   end
 end
