8459376971f1332c91025d30c1364005c785cc73
Alastair Lloyd <dex8056@aol.com>
2023-01-15 09:23:59 -0700

    add test for user story 3

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -28,6 +28,12 @@ describe "bulk discounts index" do
   it 'for each discount id it is a link to the discount show page' do
     visit merchant_bulk_discounts_path(@merchant1)
 
+    within("#discounts") do
+      expect(page).to have_link("#{@bd1.id}")
+      expect(page).to have_link("#{@bd2.id}")
+      expect(page).to_not have_link("#{@bd3.id}")
+    end
+
     click_link "Discount No #{@bd1.id}"
 
     expect(page).to have_current_path("/merchant/#{@merchant1.id}/bulk_discounts.#{@bd1.id}")
@@ -39,5 +45,29 @@ describe "bulk discounts index" do
     click_link 'Create New Discount'
 
     expect(page).to have_current_path("/merchant/#{@merchant1.id}/bulk_discounts/new")
+  end
+
+  it 'has a button to delete a discount for that merchant' do
+    visit merchant_bulk_discounts_path(@merchant1)
+
+    within("#discounts") do
+      expect(page).to have_button("Delete Discount: #{@bd1.id}")
+      expect(page).to have_button("Delete Discount: #{@bd2.id}")
+      expect(page).to_not have_button("Delete Discount: #{@bd3.id}")
+
+      click_on "Delete Discount: #{@bd1.id}"
+    end
+
+    expect(current_path).to eq(merchant_bulk_discounts_path(@merchant1))
+
+    within("#discounts") do
+      expect(page).to_not have_content("Percentage Discount: #{@bd1.percentage}")
+      expect(page).to_not have_content("Quantity Threshold: #{@bd1.quantity_threshold}")  
+      expect(page).to_not have_button("Delete Discount: #{@bd1.id}")
+      expect(page).to have_content("Percentage Discount: #{@bd2.percentage}")
+      expect(page).to have_content("Quantity Threshold: #{@bd2.quantity_threshold}")
+      expect(page).to have_button("Delete Discount: #{@bd2.id}")
+      expect(page).to_not have_button("Delete Discount: #{@bd3.id}")
+    end
   end
 end
