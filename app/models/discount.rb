0ab3b64bd719c3f6d4cc6f9408fb726684f66775
Kelsie Porter <krporter22@gmail.com>
2023-01-15 13:58:32 -0700

    Add relationships to discount and model tests

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,4 +2,10 @@ class Discount < ApplicationRecord
   validates_presence_of :percentage
   validates_presence_of :threshold
   belongs_to :merchant
+  has_many :items, through: :merchant
+  has_many :invoice_items, through: :items
+
+  def qualifying_invoice_items 
+    joins(merchants: [:items, :invoice_items])
+  end
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -8,5 +8,7 @@ describe Discount do
 
   describe "relationships" do
     it { should belong_to :merchant }
+    it { should have_many(:items).through(:merchant)}
+    it { should have_many(:invoice_items).through(:items)}
   end
 end
