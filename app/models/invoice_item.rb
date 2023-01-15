1bb63bb794df14bf783df9db45bd41e91794ae15
Kelsie Porter <krporter22@gmail.com>
2023-01-15 13:21:56 -0700

    Create invoice_item instance methods and model tests

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -14,4 +14,18 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  def total_price
+    quantity * unit_price
+  end
+
+  def discounted_total_price(discount)
+    if quantity >= discount.threshold
+      # discounted_unit_price = unit_price * ((100.0 - discount.percentage)/100)
+      # discounted_unit_price * quantity
+      total_price * ((100.0 - discount.percentage)/100)
+    else 
+      total_price
+    end
+  end
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -39,4 +39,36 @@ RSpec.describe InvoiceItem, type: :model do
       expect(InvoiceItem.incomplete_invoices).to eq([@i1, @i3])
     end
   end
+
+  describe 'instance methods' do 
+    it 'total_price' do 
+      merchant_1 = Merchant.create!(name: 'Hair Care')
+      item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: merchant_1.id, status: 1)
+      item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: merchant_1.id)
+      customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      invoice_1 = Invoice.create!(customer_id: customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      ii_1 = InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 9, unit_price: 10, status: 2)
+      ii_11 = InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_8.id, quantity: 1, unit_price: 10, status: 1)
+      discount_1 = merchant_1.discounts.create!(threshold: 5, percentage: 20)
+      discount_2 = merchant_1.discounts.create!(threshold: 10, percentage: 50)
+
+      expect(ii_1.total_price).to eq(90.0)
+      expect(ii_11.total_price).to eq(10.0)
+    end
+
+    it 'discounted_total_price' do 
+      merchant_1 = Merchant.create!(name: 'Hair Care')
+      item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: merchant_1.id, status: 1)
+      item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: merchant_1.id)
+      customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      invoice_1 = Invoice.create!(customer_id: customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      ii_1 = InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 9, unit_price: 10, status: 2)
+      ii_11 = InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_8.id, quantity: 1, unit_price: 10, status: 1)
+      discount_1 = merchant_1.discounts.create!(threshold: 5, percentage: 20)
+      discount_2 = merchant_1.discounts.create!(threshold: 10, percentage: 50)
+
+      expect(ii_1.discounted_total_price(discount_1)).to eq(72.0)
+      expect(ii_11.discounted_total_price(discount_2)).to eq(ii_11.total_price)
+    end
+  end
 end
