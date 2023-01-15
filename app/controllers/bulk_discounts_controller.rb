8455fa9207434e1c6efbb959ff0a81ee672adc66
Braxton Eddings <beddings81@gmail.com>
2023-01-15 00:47:52 -0600

    Complete user story 2

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,4 +6,14 @@ class BulkDiscountsController < ApplicationController
   def show
     
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    BulkDiscount.create!(percent_discount: params[:percent], quantity_threshold: params[:quantity], merchant_id: merchant.id)
+    redirect_to merchant_bulk_discounts_path(merchant)
+  end
 end
