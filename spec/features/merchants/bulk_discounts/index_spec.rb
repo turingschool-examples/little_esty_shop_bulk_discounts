557f3587dda87ce0d0da5d9af076ad6b1ab48514
Anhnhi Tran <anhtran811@gmail.com>
2023-01-15 11:32:44 -0700

    Adds within block

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -90,14 +90,15 @@ RSpec.describe 'Bulk Discount Index Page' do
     describe 'User Story 9' do
       it 'displays the next 3 upcoming holidays' do
         merchant_1 = create(:merchant)
+        three_holidays = HolidayService.name_and_date_next_three_holidays
 
         visit merchant_bulk_discounts_path(merchant_1)
 
-        three_holidays = HolidayService.name_and_date_next_three_holidays
-
-        expect(page).to have_content("Upcoming Holidays")
-        three_holidays.each do |holiday|
-          expect(page).to have_content(holiday)
+        within("#holidays-") do
+          expect(page).to have_content("Upcoming Holidays")
+          three_holidays.each do |holiday|
+            expect(page).to have_content(holiday)
+          end
         end
       end
     end
