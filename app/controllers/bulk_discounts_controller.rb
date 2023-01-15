3b568d325d2898850cfeef5494bf0be440163878
Anhnhi Tran <anhtran811@gmail.com>
2023-01-14 17:05:46 -0700

    Completes user story 4

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -4,6 +4,8 @@ class BulkDiscountsController < ApplicationController
   end
 
   def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
   end
   
   def new
---
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Bulk Discounts Show Page' do
+  describe 'User story 4' do
+# As a merchant
+# When I visit my bulk discount show page
+# Then I see the bulk discount's quantity threshold and percentage discount
+    it 'displays the bulk discount quantity and percentage' do
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+
+      bulk_discount_1 = merchant_1.bulk_discounts.create!(quantity_threshold: 10, percentage: 5)
+      bulk_discount_2 = merchant_2.bulk_discounts.create!(quantity_threshold: 15, percentage: 15)
+
+      visit merchant_bulk_discount_path(merchant_1, bulk_discount_1.id)
+
+      expect(page).to have_content(bulk_discount_1.id)
+      expect(page).to have_content(bulk_discount_1.quantity_threshold)
+      expect(page).to have_content(bulk_discount_1.percentage)
+      expect(page).to_not have_content(bulk_discount_2.id)
+      expect(page).to_not have_content(bulk_discount_2.quantity_threshold)
+      expect(page).to_not have_content(bulk_discount_2.percentage)
+    end
+  end
+end
