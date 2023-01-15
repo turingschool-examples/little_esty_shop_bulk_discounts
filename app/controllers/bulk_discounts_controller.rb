bedef247965e76caaef1a75f78579f1f129bb9c5
Braxton Eddings <beddings81@gmail.com>
2023-01-15 01:29:11 -0600

    Pass test for a link to delete a discount on the discounts index page

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -16,4 +16,12 @@ class BulkDiscountsController < ApplicationController
     BulkDiscount.create!(percent_discount: params[:percent], quantity_threshold: params[:quantity], merchant_id: merchant.id)
     redirect_to merchant_bulk_discounts_path(merchant)
   end
+
+  def destroy
+    bulk_discount = BulkDiscount.find(params[:id])
+    bulk_discount.destroy
+    merchant = Merchant.find(params[:merchant_id])
+
+    redirect_to merchant_bulk_discounts_path(merchant)
+  end
 end
