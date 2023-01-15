76300ff43d87e6575db88080159c013e43070bee
Kelsie Porter <krporter22@gmail.com>
2023-01-15 12:07:44 -0700

    Add discounts relationship and associated test

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,10 +7,21 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :discounts, through: :merchants
 
   enum status: [:cancelled, 'in progress', :completed]
 
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+  # def total_discounted_revenue
+  #   # require 'pry'; binding.pry
+  #   individual_discounted_prices = []
+  #   invoice_items.each do |item|
+  #     if item.quantity 
+
+  #     end
+  #   end
+  # end
 end
