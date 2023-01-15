dbbdadba61b1a284d8844e12ebaf592e2d06a4cd
Anhnhi Tran <anhtran811@gmail.com>
2023-01-14 18:17:57 -0700

    Edit bulk item tests passing

--- spec/features/merchants/bulk_discounts/edit_spec.rb
+++ spec/features/merchants/bulk_discounts/edit_spec.rb
@@ -27,10 +27,19 @@ RSpec.describe 'Edit Bulk Discount' do
       click_button('Update bulk discount')
       
       expect(current_path).to eq(merchant_bulk_discount_path(merchant_1, bulk_discount_1.id))
-      expect(page).to_not have_content("Quantity Threshold: 10")
       expect(page).to have_content("Quantity Threshold: 20")
-      expect(page).to have_content("Quantity Threshold: #{bulk_discount_1.percentage}")
+      expect(page).to have_content("Percentage: #{bulk_discount_1.percentage}")
+      expect(page).to_not have_content("Quantity Threshold: 10")
       
+      click_link('Edit bulk discount')
+      
+      fill_in('percentage', with: 15)
+      click_button('Update bulk discount')
+      
+      expect(current_path).to eq(merchant_bulk_discount_path(merchant_1, bulk_discount_1.id))
+      expect(page).to have_content("Percentage: 15")
+      expect(page).to have_content("Quantity Threshold: 20")
+      expect(page).to_not have_content("Percentage: 5")
     end
   end
 end
