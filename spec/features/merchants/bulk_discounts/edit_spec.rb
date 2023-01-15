81a995d5e43bf7964b6db3ddd290d34555ad84de
Anhnhi Tran <anhtran811@gmail.com>
2023-01-14 18:59:14 -0700

    Removes commented out lines

--- spec/features/merchants/bulk_discounts/edit_spec.rb
+++ spec/features/merchants/bulk_discounts/edit_spec.rb
@@ -2,15 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Edit Bulk Discount' do
   describe 'User story 5' do
-# As a merchant
-# When I visit my bulk discount show page
-# Then I see a link to edit the bulk discount
-# When I click this link
-# Then I am taken to a new page with a form to edit the discount
-# And I see that the discounts current attributes are pre-poluated in the form
-# When I change any/all of the information and click submit
-# Then I am redirected to the bulk discount's show page
-# And I see that the discount's attributes have been updated
     it 'has a pre-populated form to edit the discount' do
       merchant_1 = create(:merchant)
 
