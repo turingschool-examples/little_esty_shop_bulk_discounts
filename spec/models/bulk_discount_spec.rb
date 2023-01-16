9e00f86ce5f7d21d038121aa84e1ea8c0cd6a88d
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-16 11:55:58 -0600

    add bulk_discount getter and setter methods for sanitized_percentage

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe BulkDiscount do
   describe 'relationships' do
     it { should belong_to :merchant}
   end
+
+  describe 'getter/setter methods' do
+    describe '#sanitized_percentage' do
+      it 'returns santized percentages' do
+        merchant = Merchant.create!(name: 'Merchant')
+        discount = merchant.bulk_discounts.create!(percentage_discount: 0.75, quantity_threshold: 10)
+        expect(discount.sanitized_percentage).to eq(75)
+        discount.sanitized_percentage=(22)
+        expect(discount.percentage_discount).to eq(0.22)
+      end
+    end
+  end
 end
