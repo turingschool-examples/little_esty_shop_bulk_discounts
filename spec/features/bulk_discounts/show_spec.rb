6bc5f26d32966066242550d5324c5f2e9a3f4559
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-16 14:21:06 -0600

    add link to edit form from bulk edit show page

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -16,4 +16,24 @@ RSpec.describe 'bulk discount show page' do
     expect(page).to have_content("Percentage Discount: #{@discount1a.sanitized_percentage}%")
     expect(page).to have_content("Quantity Threshold: #{@discount1a.quantity_threshold}")
   end
+
+  # 5: Merchant Bulk Discount Edit
+
+  # As a merchant
+  # When I visit my bulk discount show page
+  # Then I see a link to edit the bulk discount
+  # When I click this link
+  # Then I am taken to a new page with a form to edit the discount
+  # And I see that the discounts current attributes are pre-poluated in the form
+  # When I change any/all of the information and click submit
+  # Then I am redirected to the bulk discount's show page
+  # And I see that the discount's attributes have been updated
+
+  it 'has a link to the bulk discount edit page' do
+    expect(page).to have_link('Edit Bulk Discount')
+
+    click_link('Edit Bulk Discount')
+    
+    expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant1, @discount1a))
+  end
 end
