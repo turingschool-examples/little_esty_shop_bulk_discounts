a54d8c13392ad48c92085a498d29e6689de8a544
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-16 11:55:27 -0600

    add bulkdiscount model validation ranges on percentage_discount and quantity_threshold

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,5 +1,5 @@
 class BulkDiscount < ApplicationRecord
-  validates :percentage_discount, presence: true, numericality: true
-  validates :quantity_threshold, presence: true, numericality: {only_integer: true}
+  validates :percentage_discount, presence: true, numericality: {less_than: 1, greater_than: 0}
+  validates :quantity_threshold, presence: true, numericality: {only_integer: true, greater_than: 0}
   belongs_to :merchant
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe BulkDiscount do
   describe 'validations' do
     it { should validate_presence_of :percentage_discount }
-    it { should validate_numericality_of :percentage_discount }
+    it { should validate_numericality_of(:percentage_discount).is_less_than(1).is_greater_than(0) }
     it { should validate_presence_of :quantity_threshold }
-    it { should validate_numericality_of(:quantity_threshold).only_integer }
+    it { should validate_numericality_of(:quantity_threshold).only_integer.is_greater_than(0) }
   end
 
   describe 'relationships' do
