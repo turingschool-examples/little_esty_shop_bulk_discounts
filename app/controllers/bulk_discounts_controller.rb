e3ca0feee6d96d48acbf42969bfce81159dc5813
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-16 12:00:04 -0600

    add bulk discounts controller create action with happy and sad paths

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -11,4 +11,15 @@ class BulkDiscountsController < ApplicationController
     merchant = Merchant.find(params[:merchant_id])
     @bulk_discount = merchant.bulk_discounts.new
   end
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = merchant.bulk_discounts.new(bulk_discount_params)
+    if @bulk_discount.save
+      flash[:notice] = 'New Bulk Discount was successfully created'
+      redirect_to merchant_bulk_discounts_path(merchant)
+    else
+      flash[:alert] = "New Bulk Discount Creation Failed!"
+      render :new
+    end
+  end
 end
