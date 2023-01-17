c2cada414da07b83a299ba1e8411f9d4e76fdef2
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-01-16 20:02:48 -0600

    split up total discounted revenue into helper methods

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -14,4 +14,22 @@ class Invoice < ApplicationRecord
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+  def all_item_discount_savings
+    # require 'pry'; binding.pry
+    self
+    .invoice_items
+    .joins(:bulk_discounts)
+    .select('invoice_items.id, (max(invoice_items.quantity * invoice_items.unit_price * bulk_discounts.percentage_discount)) as max_savings, max(bulk_discounts.percentage_discount) as max_discount')
+    .where('invoice_items.quantity >= bulk_discounts.quantity_threshold')
+    .group(:id)
+  end
+
+  def total_savings
+    all_item_discount_savings.sum(&:max_savings)
+  end
+
+  def total_discounted_revenue
+    total_revenue - total_savings
+  end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -154,5 +154,50 @@ RSpec.describe Invoice, type: :model do
       end
     end
 
+    describe '#total_savings' do
+      it 'returns the integer value of the total savings on an invoice' do
+        @merchant1 = Merchant.create!(name: 'Hair Care')
+        @bulk_discount1a = @merchant1.bulk_discounts.create!(percentage_discount: 0.2, quantity_threshold: 10)
+        @bulk_discount1b = @merchant1.bulk_discounts.create!(percentage_discount: 0.3, quantity_threshold: 15)
+        @item_1a = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+        @item_1b = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+        
+        @merchant2 = Merchant.create!(name: 'Melon Care')
+        @item_2a = Item.create!(name: "Melon Scooper", description: "Scoops melons", unit_price: 5, merchant_id: @merchant2.id)
+  
+        @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+        @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+  
+        @ii_1a = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1a.id, quantity: 12, unit_price: 10, status: 2)
+        @ii_1b = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1b.id, quantity: 15, unit_price: 10, status: 1)
+
+        @ii_2a = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2a.id, quantity: 15, unit_price: 10, status: 1)
+
+        expect(@invoice_1.total_savings).to eq(69.0)
+      end
+    end
+
+    describe '#total_discounted_revenue' do
+      it 'returns the total discounted revenue of an invoice' do
+        @merchant1 = Merchant.create!(name: 'Hair Care')
+        @bulk_discount1a = @merchant1.bulk_discounts.create!(percentage_discount: 0.2, quantity_threshold: 10)
+        @bulk_discount1b = @merchant1.bulk_discounts.create!(percentage_discount: 0.3, quantity_threshold: 15)
+        @item_1a = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+        @item_1b = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+        
+        @merchant2 = Merchant.create!(name: 'Melon Care')
+        @item_2a = Item.create!(name: "Melon Scooper", description: "Scoops melons", unit_price: 5, merchant_id: @merchant2.id)
+  
+        @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+        @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+  
+        @ii_1a = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1a.id, quantity: 12, unit_price: 10, status: 2)
+        @ii_1b = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1b.id, quantity: 15, unit_price: 10, status: 1)
+
+        @ii_2a = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2a.id, quantity: 15, unit_price: 10, status: 1)
+
+        expect(@invoice_1.total_discounted_revenue).to eq(351.0)
+      end
+    end
   end
 end
