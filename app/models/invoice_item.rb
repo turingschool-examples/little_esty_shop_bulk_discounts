e289a11b7faeb1952416bafe1ea0b021a4a40b33
Matisse Mallette <matissemallette@gmail.com>
2023-01-18 03:21:05 -0700

    Complete user story 7

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -16,4 +16,16 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  def applied_discount(merchant)
+    bulk_discount = bulk_discounts
+    .where('threshold < ? AND bulk_discounts.merchant_id = ?', quantity, merchant.id)
+    .group(:id)
+    .order('bulk_discounts.threshold DESC')
+    .first
+    
+    #binding.pry
+    
+    return bulk_discount
+  end
 end
