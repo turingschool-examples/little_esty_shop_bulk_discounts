68d1f458d7536a519c4ae8d588e5d8016e2805b6
Adam Bailey <ab67319@gmail.com>
2023-03-03 12:41:56 -0700

    feat: create functionality for User Story 2, merchatn bulk discount create

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,4 +6,27 @@ class BulkDiscountsController < ApplicationController
 
   def show
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = @merchant.bulk_discounts.new
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = merchant.bulk_discounts.new(bulk_discount_params)
+
+    if @bulk_discount.save
+      flash[:notice] = "New bulk discount added"
+      redirect_to merchant_bulk_discounts_path(params[:merchant_id])
+    else
+      flash[:notice] = "Invalid form: Unable to create Bulk Discount"
+      redirect_to new_merchant_bulk_discount_path(params[:merchant_id])
+    end
+  end
+  
+  private
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:quantity_threshold, :percentage_discount, :merchant_id)
+  end
 end
