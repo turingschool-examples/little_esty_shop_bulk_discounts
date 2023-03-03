8684f97946f1d8f30e76c8e7725a148d38f47a1d
Adam Bailey <ab67319@gmail.com>
2023-03-02 18:26:28 -0700

    feat: create functionality for user story 1, merchant bulk discounts index

---
+++ app/controllers/bulk_discounts_controller.rb
@@ -0,0 +1,9 @@
+class BulkDiscountsController < ApplicationController
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discounts = @merchant.bulk_discounts
+  end
+
+  def show
+  end
+end
