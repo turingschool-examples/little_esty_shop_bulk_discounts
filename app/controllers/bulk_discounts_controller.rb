b4e4bc6f9b44e2aa7ab20a34df12e979b1437bb3
Elle Majors <hellolovely@ellemajors.com>
2023-03-03 15:36:26 -0700

    test: user story one, views and controllers for bulk discounts

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,5 +1,13 @@
 class BulkDiscountsController < ApplicationController
+  before_action :set_merchant
+  
   def index
     @bulk_discounts = @merchant.bulk_discounts
+  #   require 'pry'; binding.pry
+  end
+
+  private
+  def set_merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -34,4 +34,11 @@ FactoryBot.define do
     merchant
     invoice
   end
+
+  factory :bulk_discount do
+    name { Faker::Marketing.buzzwords + " discount" }
+    percentage { [10, 20, 30].sample }
+    quantity_threshold { [2, 3, 5].sample }
+    merchant
+  end
 end
