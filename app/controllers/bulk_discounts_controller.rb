6be022877e52a945220db1784f09fad5da77d97c
Brian Hayes <Brian.hayes1200@gmail.com>
2023-03-03 15:11:30 -0700

    Feat: add funtionality to fill out form and create a new bulk discount that is displayed on the bulk discount index page

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -9,6 +9,18 @@ class BulkDiscountsController < ApplicationController
   end
   
   def new
-    
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+
+    bulk_discount = BulkDiscount.create!(bulk_discount_params)
+    redirect_to  merchant_bulk_discounts_path(@merchant)
+  end
+
+  private
+  def bulk_discount_params
+    params.permit(:percentage, :quantity_threshhold, :merchant_id)
   end
 end
