4f06481179bd9841abf5cc1afa204b72da5f058f
Adam Bailey <ab67319@gmail.com>
2023-03-03 12:40:48 -0700

    feat: create model validations

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,13 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+
+  validates_presence_of :quantity_threshold, :percentage_discount
+
+  validates_numericality_of :quantity_threshold, only_integer: true
+  validates_numericality_of :quantity_threshold, greater_than: 0
+
+  validates_numericality_of :percentage_discount, less_than: 1
+  validates_numericality_of :percentage_discount, greater_than: 0
+
+
 end
