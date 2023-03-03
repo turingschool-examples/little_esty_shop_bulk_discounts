58491b3ff0ec1cf5eb166937f7563a860adbcc8e
Caleb Thomas <cjthomas00@comcast.net>
2023-03-03 09:38:13 -0700

    created index test for US 1.

---
+++ spec/features/bulk_discounts/index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Bulk Discount Index Page', type: :feature do
+  before :each do
+    @merchant = Merchant.create!(name: 'Hair Care')
+    @discount_1 = @merchant.bulk_discounts.create!(percentage_discount: 20, quantity_threshold: 20)
+    @discount_2 = @merchant.bulk_discounts.create!(percentage_discount: 30, quantity_threshold: 30)
+    @discount_3 = @merchant.bulk_discounts.create!(percentage_discount: 40, quantity_threshold: 40)
+    visit merchant_dashboard_index_path(@merchant)
+  end
+  describe 'As a merchant' do
+    describe 'When I visit my merchant index page' do
+      it " I see a link to view all my discounts" do
+        expect(page).to have_link("My Discounts")
+        click_link("My Discounts")
+        expect(current_path).to eq(merchant_bulk_discounts_path(@merchant))
+      end
+
+      it "Then I am taken to my bulk discounts index page Where I see all of my bulk discounts including their percentage discount and quantity thresholds And each bulk discount listed includes a link to its show page" do
+        click_link("My Discounts")
+        expect(page).to have_content(@discount_1.percentage_discount)
+        expect(page).to have_content(@discount_1.quantity_threshold)
+        expect(page).to have_content(@discount_2.percentage_discount)
+        expect(page).to have_content(@discount_2.quantity_threshold)
+        expect(page).to have_content(@discount_3.percentage_discount)
+        expect(page).to have_content(@discount_3.quantity_threshold)
+        expect(page).to have_link(@discount_1.id)
+        expect(page).to have_link(@discount_2.id)
+        expect(page).to have_link(@discount_3.id)
+      end
+    end
+  end
+end
