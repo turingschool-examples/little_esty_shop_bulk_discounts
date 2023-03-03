49603c389a62118aa28d8a0affae32d998875f46
Chris Crane <chrisjcrane@gmail.com>
2023-03-02 17:16:47 -0700

    Create BulkDiscount with spec and associate to Merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,13 +4,14 @@ describe Merchant do
   describe "validations" do
     it { should validate_presence_of :name }
   end
+
   describe "relationships" do
     it { should have_many :items }
     it { should have_many(:invoice_items).through(:items) }
     it {should have_many(:invoices).through(:invoice_items)}
     it { should have_many(:customers).through(:invoices) }
     it { should have_many(:transactions).through(:invoices) }
-
+    it { should have_many :bulk_discounts }
   end
 
   describe "class methods" do
