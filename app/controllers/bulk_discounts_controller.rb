edc1b0f8736ca91e777e6159dc95d994f90457a2
Harrison Ryan <hwryan12@gmail.com>
2023-03-02 18:57:04 -0600

    feature: Complete user story 1

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,7 +1,14 @@
 class BulkDiscountsController < ApplicationController
+  before_action :find_merchant
+  
   def index
     # binding.pry
-    @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.bulk_discounts
+  end
+  
+  private 
+  
+  def find_merchant   
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
