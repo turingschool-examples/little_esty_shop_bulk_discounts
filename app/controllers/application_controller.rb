4705fb73c42ad54e25be27e0c3086a47d2e2a7e2
Jade Stewart <jadekstewart3@gmail.com>
2023-03-03 12:58:16 -0700

    Add new and create action to bulk discounts controller add error_message to application controller for display of error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+add_flash_types :error
+
+  def error_message(errors)
+    errors.full_messages.join(', ')
+  end
 end
--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -3,4 +3,25 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @bulk_discounts = @merchant.bulk_discounts
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @new_bulk_discount = @merchant.bulk_discounts.new(bulk_discount_params)
+    if @new_bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(@merchant)
+    else 
+      flash[:error] = @new_bulk_discount.errors.full_messages.to_sentence
+      redirect_to new_merchant_bulk_discount_path(@merchant)
+    end
+  end
+
+  private
+
+  def bulk_discount_params
+    params.permit(:name, :percentage_discount, :quantity_threshold, :merchant_id)
+  end
 end
