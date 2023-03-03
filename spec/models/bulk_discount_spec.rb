f8daf42cda85fd8d3799597dc67c8ff6a73cd6e9
Adam Bailey <ab67319@gmail.com>
2023-03-03 12:40:39 -0700

    test: create model validation tests

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe BulkDiscount, type: :model do
   describe 'relationships' do
     it { should belong_to :merchant }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :quantity_threshold }
+    it { should validate_presence_of :percentage_discount }
+
+    it { should validate_numericality_of(:quantity_threshold).only_integer }
+    it { should validate_numericality_of(:quantity_threshold).is_greater_than(0) }
+
+    it { should validate_numericality_of(:percentage_discount).is_greater_than(0) }
+    it { should validate_numericality_of(:percentage_discount).is_less_than(1) }
+  end
 end
