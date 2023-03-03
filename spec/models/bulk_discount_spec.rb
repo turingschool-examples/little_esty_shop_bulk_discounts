42231cd4ec86ec58b56cbd115ab9f738a3429df2
Jade Stewart <jadekstewart3@gmail.com>
2023-03-03 13:03:03 -0700

    Add validation tests to bulk discounts model

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
  it { should belong_to :merchant }
+ it  { should validate_presence_of :name}
+ it  { should validate_presence_of :percentage_discount}
+ it  { should validate_presence_of :quantity_threshold}
 end
