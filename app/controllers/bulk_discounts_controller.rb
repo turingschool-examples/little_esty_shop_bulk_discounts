bd5ac4456ba7d637577786a45f2e183104ff9644
Adam Bailey <ab67319@gmail.com>
2023-03-03 16:46:05 -0700

    feat: create functionality for user story 3, merchant bulk discount delete

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,32 +1,48 @@
 class BulkDiscountsController < ApplicationController
+  before_action :find_merchant, only: [:index, :new, :create]
+  before_action :find_bulk_discount, only: [:destroy, :show]
+  
   def index
-    @merchant = Merchant.find(params[:merchant_id])
     @bulk_discounts = @merchant.bulk_discounts
   end
 
   def show
+
   end
 
   def new
-    @merchant = Merchant.find(params[:merchant_id])
     @bulk_discount = @merchant.bulk_discounts.new
   end
 
   def create
-    merchant = Merchant.find(params[:merchant_id])
-    @bulk_discount = merchant.bulk_discounts.new(bulk_discount_params)
+    @bulk_discount = @merchant.bulk_discounts.new(bulk_discount_params)
 
     if @bulk_discount.save
       flash[:notice] = "New bulk discount added"
       redirect_to merchant_bulk_discounts_path(params[:merchant_id])
     else
       flash[:notice] = "Invalid form: Unable to create Bulk Discount"
+      # render new page
       redirect_to new_merchant_bulk_discount_path(params[:merchant_id])
     end
+  end
+
+  def destroy
+    @bulk_discount.destroy
+
+    redirect_to merchant_bulk_discounts_path(params[:merchant_id])
   end
   
   private
   def bulk_discount_params
-    params.require(:bulk_discount).permit(:quantity_threshold, :percentage_discount, :merchant_id)
+    params.require(:bulk_discount).permit(:quantity_threshold, :percentage_discount)
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def find_bulk_discount
+    @bulk_discount = BulkDiscount.find(params[:id])
   end
 end
