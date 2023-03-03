e227020290d11bbf828a625035a772a22953abfb
Caleb Thomas <cjthomas00@comcast.net>
2023-03-03 10:23:43 -0700

    add #new and #create actions to controller.

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -3,4 +3,20 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @bulk_discounts = @merchant.bulk_discounts
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.bulk_discounts.create(new_discount_attributes)
+    @discount.save
+    redirect_to merchant_bulk_discounts_path(@merchant)
+  end
+
+  private
+  def new_discount_attributes
+    params.permit(:percentage_discount, :quantity_threshold)
+  end
 end
