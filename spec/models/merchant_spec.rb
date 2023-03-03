b0384b29aa2222807330439cd78111e38ec67e3f
Harrison Ryan <hwryan12@gmail.com>
2023-03-02 18:26:30 -0600

    feature: Add has_many bulk_discounts to merchant model and spec file

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,9 +5,10 @@ describe Merchant do
     it { should validate_presence_of :name }
   end
   describe "relationships" do
+    it { should have_many :bulk_discounts }
     it { should have_many :items }
     it { should have_many(:invoice_items).through(:items) }
-    it {should have_many(:invoices).through(:invoice_items)}
+    it { should have_many(:invoices).through(:invoice_items) }
     it { should have_many(:customers).through(:invoices) }
     it { should have_many(:transactions).through(:invoices) }
 
