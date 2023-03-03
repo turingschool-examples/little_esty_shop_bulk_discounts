69de0839ed337ebd8d632bc029c3a106172db58d
Matt Enyeart <menyeart1@gmail.com>
2023-03-03 09:27:14 -0700

    Add logic to bulk discount controller create method and private bulk discount params

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,6 +6,26 @@ class BulkDiscountsController < ApplicationController
   end
 
   def show
-   @bulk_discount = BulkDiscount.find(params[:id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  def new
+    # @bulk_discount = BulkDiscount.new
+  end
+
+  def create
+    @bulk_discount = BulkDiscount.new(bulk_discount_params)
+    if @bulk_discount.save
+      redirect_to merchant_bulk_discounts_path
+    else
+      flash[:notice] = 'Invalid input'
+      render :new
+    end
+  end
+
+
+  private
+  def bulk_discount_params
+    params.permit(:percentage_discount, :quantity_threshold, :merchant_id)
   end
 end
