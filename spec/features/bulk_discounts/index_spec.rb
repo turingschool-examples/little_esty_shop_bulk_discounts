49e4ed2b9f91610844ceafbe9a6b7438e6da6855
Brian Hayes <Brian.hayes1200@gmail.com>
2023-03-02 19:47:16 -0700

    Feat: add functionality to display each discount on the discount index page

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -5,12 +5,14 @@ RSpec.describe 'bulk discount index page' do
     @merchant1 = Merchant.create!(name: 'Hair Care')
     @bulk_discount_1 = BulkDiscount.create!(percentage: 0.10, quantity_threshhold: 10, merchant: @merchant1)
     @bulk_discount_2 = BulkDiscount.create!(percentage: 0.15, quantity_threshhold: 15, merchant: @merchant1)
+
+    visit merchant_bulk_discounts_path(@merchant1)
   end
 
   it 'will have a list of all discounts for that merchant' do 
-    expect(page).to have_content("Discount: #{@bulk_discount_1.percentage}")
-    expect(page).to have_content("Amount of items needed for discount: #{@bulk_discount_1.threshold}")
-    expect(page).to have_content("Discount: #{@bulk_discount_2.percentage}")
-    expect(page).to have_content("Amount of items needed for discount: #{@bulk_discount_2.threshold}")
+    expect(page).to have_content("Discount: %10")
+    expect(page).to have_content("Amount of items needed for discount: #{@bulk_discount_1.quantity_threshhold}")
+    expect(page).to have_content("Discount: %15")
+    expect(page).to have_content("Amount of items needed for discount: #{@bulk_discount_2.quantity_threshhold}")
   end
 end
