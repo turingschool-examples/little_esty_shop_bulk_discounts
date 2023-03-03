b9bdfac552c3c582dd12d0acfcb1900a9a4993a5
Chris Crane <chrisjcrane@gmail.com>
2023-03-02 17:19:21 -0700

    Add validations to Bulk Discount

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,4 @@
 class BulkDiscount < ApplicationRecord
+  validates_presence_of :discount_percent, :quantity_threshold
   belongs_to :merchant
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe BulkDiscount do
   describe "validations" do
-    # it { should validate_presence_of :name }
+    it { should validate_presence_of :discount_percent }
+    it { should validate_presence_of :quantity_threshold }
   end
 
   describe "relationships" do
