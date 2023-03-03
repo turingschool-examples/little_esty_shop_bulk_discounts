21f63022a0787e7cb4643fe27e0e676594b66005
Harrison Ryan <hwryan12@gmail.com>
2023-03-03 14:40:34 -0600

    feature: Add validations to bulk_discount model and test/Completes user story 2

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,6 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+
+  validates :discount, presence: true
+  validates :quantity, presence: true, numericality: true
 end
--- spec/models/bulk_discount.rb
+++ spec/models/bulk_discount.rb
@@ -4,4 +4,10 @@ RSpec.describe BulkDiscount, type: :model do
   describe "relationships" do
     it { should belong_to :merchant }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:discount) }
+    it { should validate_presence_of(:quantity) }
+    it { should validate_numericality_of(:quantity) }
+  end
 end
