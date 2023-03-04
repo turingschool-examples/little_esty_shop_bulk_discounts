df26f78bb2f191922711834de8023d60eb40f56e
Weston Sandfort <sandfortw@gmail.com>
2023-03-03 17:17:15 -0700

    create bulk discount create form and update controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,5 +1,4 @@
 class BulkDiscountsController < ApplicationController
-
   def index 
     @merchant = Merchant.find(params[:merchant_id])
   end
@@ -9,6 +8,23 @@ class BulkDiscountsController < ApplicationController
   end
 
   def new
-    
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.new
+  end
+
+  def create
+    discount = BulkDiscount.new(bulk_discount_params)
+    if discount.save
+      redirect_to merchant_bulk_discounts_path(params[:merchant_id])
+    else
+      flash[:notice] = "Invalid input."
+      redirect_to  new_merchant_bulk_discount_path(params[:merchant_id])
+    end
+  end
+
+  private  
+
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:percent_discounted, :quantity_threshold, :merchant_id)
   end
 end
