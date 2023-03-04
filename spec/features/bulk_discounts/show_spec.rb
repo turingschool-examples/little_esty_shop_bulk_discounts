6807f67e1ad054d545d97da8d66c974e82452fb4
Weston Sandfort <sandfortw@gmail.com>
2023-03-03 20:33:04 -0700

    create test for show page link to edit page for bulk discount

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -22,4 +22,16 @@ describe 'bulk discounts show page' do
     expect(page).to have_content("Percentage Discount: #{@bulk_discount2.percent_discounted}")
   end
 
+  it 'has a link to an edit page for a bulk_discount (1)' do
+    expect(page).to have_link("Edit")
+    click_link("Edit")
+    expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant, @bulk_discount1))
+  end
+
+  it 'has a link to an edit page for a bulk_discount (2)' do
+    visit merchant_bulk_discount_path(@merchant, @bulk_discount2)
+    click_link("Edit")
+    expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant, @bulk_discount2))
+  end
+
 end
