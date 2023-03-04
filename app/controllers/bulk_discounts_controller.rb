dcd11f73f5818552e638e824ba9c57bf14146f81
Weston Sandfort <sandfortw@gmail.com>
2023-03-03 20:09:17 -0700

    finish user story 4

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -5,7 +5,9 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
   end
 
-  def show; end
+  def show
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
 
   def new
     @merchant = Merchant.find(params[:merchant_id])
