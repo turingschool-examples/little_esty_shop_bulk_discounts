d7e9c870c90dee814eab74a99b47ba1ce70010a1
Adam Bailey <ab67319@gmail.com>
2023-03-04 13:36:21 -0700

    feat: create functionality for user story 6, merchant invoice show page: total revenue and discounted revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,10 +7,21 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :bulk_discounts, through: :merchants
 
   enum status: [:cancelled, 'in progress', :completed]
 
   def total_revenue
     invoice_items.sum("unit_price * quantity")
+  end
+
+  def total_discounted_revenue
+    InvoiceItem.from(
+      invoice_items.joins(:bulk_discounts)
+      .select("invoice_items.*, max(bulk_discounts.percentage_discount * invoice_items.quantity * invoice_items.unit_price / 100 ) as max_discount")
+      .where("invoice_items.quantity >= bulk_discounts.quantity_threshold")
+      .group(:id)
+    )
+    .sum("max_discount")
   end
 end
