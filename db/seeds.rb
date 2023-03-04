5da86a18ed54e82f31d725dee3b5626c851958cf
Matt Enyeart <menyeart1@gmail.com>
2023-03-04 10:45:42 -0700

    Add bulk discount seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
+BulkDiscount.destroy_all
 
+@bulk_discount5 = BulkDiscount.create!(percentage_discount: 0.05, quantity_threshold: 3, merchand_id: 26)
+@bulk_discount10 = BulkDiscount.create!(percentage_discount: 0.10, quantity_threshold: 5, merchant_id: 26)
+@bulk_discount20 = BulkDiscount.create!(percentage_discount: 0.15, quantity_threshold: 10, merchant_id: 26)
