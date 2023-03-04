a2b4ecf5a68f5cc6e731b3f834bafebbfb09a76b
Jade Stewart <jadekstewart3@gmail.com>
2023-03-04 14:19:48 -0700

    Add tests for new associations

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Item, type: :model do
   describe "relationships" do
     it { should have_many(:invoices).through(:invoice_items) }
     it { should belong_to :merchant }
+    it { should have_many(:bulk_discounts).through(:merchant) }
   end
   describe "instance methods" do
     it "best day" do
