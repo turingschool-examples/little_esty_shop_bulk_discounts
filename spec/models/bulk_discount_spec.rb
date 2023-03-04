94bd6ae75e0d76e0a331cbbab44fcfa4f2a9f860
Melony Franchini <meltravelz@gmail.com>
2023-03-03 22:13:11 -0700

    Add bd new form, create action, flash messages, sad path tests, validations - US2 DONE

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,4 +4,12 @@ describe BulkDiscount, type: :model do
   describe "relationships" do
     it { should belong_to :merchant }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:percentage_discount) }
+    it { should validate_presence_of(:quantity_threshold) }
+    it { should validate_numericality_of(:percentage_discount) }
+    it { should validate_numericality_of(:quantity_threshold) }
+  end
 end
