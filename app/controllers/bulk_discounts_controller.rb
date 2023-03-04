7c850539333b2706edfef9e8c035e960dd0cd846
Weston Sandfort <sandfortw@gmail.com>
2023-03-03 19:27:06 -0700

    update controller and view to complete user story 3

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -22,6 +22,11 @@ class BulkDiscountsController < ApplicationController
     end
   end
 
+  def destroy 
+    BulkDiscount.delete(params[:id])
+    redirect_to merchant_bulk_discounts_path(params[:merchant_id])
+  end
+
   private  
 
   def bulk_discount_params
