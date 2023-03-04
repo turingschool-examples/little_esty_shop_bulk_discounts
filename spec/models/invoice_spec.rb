1ca64d572ee7a5a28630673ee2f2f57a266edd26
Matt Enyeart <menyeart1@gmail.com>
2023-03-04 10:46:56 -0700

    Add test setup for invoice model method total discounted reveneue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -20,8 +20,22 @@ RSpec.describe Invoice, type: :model do
       @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
       @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
       @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
-
       expect(@invoice_1.total_revenue).to eq(100)
     end
+
+    # it "total_discounted_revenue" do
+    #   @merchant1 = Merchant.create!(name: 'Hair Care')
+    #   @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+    #   @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+    #   @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+    #   @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+    #   @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
+    #   @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+    #   @bulk_discount5 = BulkDiscount.create!(percentage_discount: 0.05, quantity_threshold: 3, merchant: @merchant1)
+    #   @bulk_discount10 = BulkDiscount.create!(percentage_discount: 0.10, quantity_threshold: 5, merchant: @merchant1)
+    #   @bulk_discount20 = BulkDiscount.create!(percentage_discount: 0.15, quantity_threshold: 10, merchant: @merchant1)
+   
+    #   expect(@invoice_1.find_discount).to eq(@bulk_discount10)
+    # end
   end
 end
