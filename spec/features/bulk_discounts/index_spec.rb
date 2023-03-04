8c3970f3c6110f5028c59029b1af5d194ff0e408
Weston Sandfort <sandfortw@gmail.com>
2023-03-03 19:18:10 -0700

    create test for deleting a bulk discount

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'bulk discounts index' do
-  before do 
+  before do
     @merchant = create(:merchant)
     @bulk_discount1 = create(:bulk_discount, merchant_id: @merchant.id)
     @bulk_discount2 = create(:bulk_discount, merchant_id: @merchant.id)
@@ -9,11 +9,11 @@ describe 'bulk discounts index' do
     visit merchant_bulk_discounts_path(@merchant)
   end
   it 'has a header' do
-    expect(page).to have_content(@merchant.name + '\'s Bulk Discounts')
+    expect(page).to have_content("#{@merchant.name}'s Bulk Discounts")
   end
 
   it 'has the information for each bulk discount' do
-    expect(page).to have_content("Bulk Discounts")
+    expect(page).to have_content('Bulk Discounts')
     expect(page).to have_content("#{@bulk_discount1.percent_discounted}% off after #{@bulk_discount1.quantity_threshold} items purchased.")
     expect(page).to have_content("#{@bulk_discount2.percent_discounted}% off after #{@bulk_discount2.quantity_threshold} items purchased.")
     expect(page).to have_content("#{@bulk_discount3.percent_discounted}% off after #{@bulk_discount3.quantity_threshold} items purchased.")
@@ -21,21 +21,21 @@ describe 'bulk discounts index' do
 
   it 'each item has a link to its show page (1)' do
     within "div##{@bulk_discount1.id}" do
-      click_link "Show Page"
+      click_link 'Show Page'
       expect(current_path).to eq("/merchant/#{@merchant.id}/bulk_discounts/#{@bulk_discount1.id}")
     end
   end
 
   it 'each item has a link to its show page (2)' do
     within "div##{@bulk_discount2.id}" do
-      click_link "Show Page"
+      click_link 'Show Page'
       expect(current_path).to eq("/merchant/#{@merchant.id}/bulk_discounts/#{@bulk_discount2.id}")
     end
   end
 
   it 'each item has a link to its show page (3)' do
     within "div##{@bulk_discount3.id}" do
-      click_link "Show Page"
+      click_link 'Show Page'
       expect(current_path).to eq("/merchant/#{@merchant.id}/bulk_discounts/#{@bulk_discount3.id}")
     end
   end
@@ -44,5 +44,14 @@ describe 'bulk discounts index' do
     expect(page).to have_link('Create a new Bulk Discount')
     click_link 'Create a new Bulk Discount'
     expect(current_path).to eq(new_merchant_bulk_discount_path(@merchant))
+  end
+
+  it 'has a link to delete a bulk_discount' do
+    within "div##{@bulk_discount1.id}" do
+      expect(page).to have_content("#{@bulk_discount1.percent_discounted}% off after #{@bulk_discount1.quantity_threshold} items purchased.")
+      click_link 'Delete'
+      expect(current_path).to eq(merchant_bulk_discounts_path(@merchant))
+    end
+    expect(page).to_not have_content("#{@bulk_discount1.percent_discounted}% off after #{@bulk_discount1.quantity_threshold} items purchased.")
   end
 end
