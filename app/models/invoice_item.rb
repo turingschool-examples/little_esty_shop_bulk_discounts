da49063f3c9c633ee47cce20d1b282488c77117d
Jade Stewart <jadekstewart3@gmail.com>
2023-03-05 16:53:20 -0700

    Add model instance methods best_discount prediscount_revenue and total_discounted_revenue

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,11 +7,28 @@ class InvoiceItem < ApplicationRecord
 
   belongs_to :invoice
   belongs_to :item
-  has_many :bulk_discounts, through: :item
+  has_one :merchant, through: :item
+  # has_many :bulk_discounts, through: :item
+  has_many :bulk_discounts, through: :merchant
   enum status: [:pending, :packaged, :shipped]
 
   def self.incomplete_invoices
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
+  end
+
+  def best_discount
+    bulk_discounts #all bulk discounts for merchant
+      .where('quantity_threshold <= ?', quantity) #only discounts that apply to this invoice item
+      .order(percentage_discount: :desc).first #highest discount
+  end
+
+  def prediscount_revenue
+    quantity * unit_price
+  end
+
+  def total_discounted_revenue
+    prediscount_revenue * (1 - (best_discount&.percentage_discount || 0))
+    #best_discount = bulk_discount active record objects safe nav if nil
   end
 end
