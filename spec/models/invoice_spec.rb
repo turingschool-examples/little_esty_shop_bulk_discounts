b18b928068408189a882a41182004899cc053b9a
Caleb Thomas <cjthomas00@comcast.net>
2023-03-05 15:03:37 -0700

    create model test for #discounted_revenue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -24,5 +24,19 @@ RSpec.describe Invoice, type: :model do
 
       expect(@invoice_1.total_revenue).to eq(100)
     end
+
+    it "total_discounted_revenue" do
+      merchant1 = Merchant.create!(name: 'Hair Care')
+      item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: merchant1.id, status: 1)
+      item_2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: merchant1.id)
+      customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      invoice_1 = Invoice.create!(customer_id: customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      ii_1 = InvoiceItem.create!(invoice_id: invoice_1.id, item_id: item_1.id, quantity: 19, unit_price: 10, status: 2)
+      bulk_discount_1 = merchant1.bulk_discounts.create!(percentage_discount: 5.0, quantity_threshold: 5)
+      bulk_discount_2 = merchant1.bulk_discounts.create!(percentage_discount: 10.0, quantity_threshold: 15)
+      bulk_discount_3 = merchant1.bulk_discounts.create!(percentage_discount: 15.0, quantity_threshold: 20)
+      expect(invoice_1.total_revenue).to eq(190)
+      expect(invoice_1.discounted_revenue).to eq(171.0)
+    end
   end
 end
