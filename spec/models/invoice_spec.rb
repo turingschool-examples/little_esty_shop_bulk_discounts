d55c03b05583bbe08e40f7f732d0598a36e3c1e6
Weston Sandfort <sandfortw@gmail.com>
2023-03-04 17:54:33 -0700

    add more tests for discount_total_revenue method

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -13,28 +13,55 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:merchants).through(:items) }
     it { should have_many :transactions }
     it { should have_many(:bulk_discounts).through(:merchants) }
-
   end
+
   describe 'instance methods' do
     before do
       @merchant1 = Merchant.create!(name: 'Hair Care')
       @item_1 = Item.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 10, merchant_id: @merchant1.id, status: 1)
-      @item_8 = Item.create!(name: 'Butterfly Clip', description: 'This holds up your hair but in a clip', unit_price: 5, merchant_id: @merchant1.id)
+      @item_2 = Item.create!(name: 'Butterfly Clip', description: 'This holds up your hair but in a clip', unit_price: 5, merchant_id: @merchant1.id)
       @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
       @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: '2012-03-27 14:54:09')
-      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
-      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+      @invoice_item1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
+      @invoice_item2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 1, unit_price: 10, status: 1)
     end
     it 'total_revenue' do
       expect(@invoice_1.total_revenue).to eq(100)
     end
 
-    describe '#discount_total_revenue'
-      before do
-        @bulk_discount1 = BulkDiscount.create!(percent_discounted: 50, quantity_threshold: 5, merchant_id: @merchant1.id)
+    describe '#discount_total_revenue' do
+      describe 'gives a discounted total for a merchant, based on quantity of invoice items, and percent off' do
+        it 'Only discounts invoice_item 1, but still includes 10 cents from invoice_item 2' do
+          BulkDiscount.create!(percent_discounted: 50, quantity_threshold: 5, merchant_id: @merchant1.id)
+          expect(@invoice_1.discount_total_revenue).to eq(55)
+        end
+
+        it 'does not change total revenue if neither of the invoice items meet threshold ' do
+          BulkDiscount.create!(percent_discounted: 50, quantity_threshold: 10, merchant_id: @merchant1.id)
+          expect(@invoice_1.discount_total_revenue).to eq(100)
+        end
+
+        it 'only gives the highest bulk discount available' do
+          BulkDiscount.create!(percent_discounted: 10, quantity_threshold: 1, merchant_id: @merchant1.id)
+          BulkDiscount.create!(percent_discounted: 40, quantity_threshold: 1, merchant_id: @merchant1.id)
+          BulkDiscount.create!(percent_discounted: 20, quantity_threshold: 1, merchant_id: @merchant1.id)
+          expect(@invoice_1.discount_total_revenue).to eq(60)
+        end
+
+        it 'combination of bulk discounts (each item has a different discount)' do
+          BulkDiscount.create!(percent_discounted: 10, quantity_threshold: 1, merchant_id: @merchant1.id)
+          BulkDiscount.create!(percent_discounted: 40, quantity_threshold: 5, merchant_id: @merchant1.id)
+          expect(@invoice_1.discount_total_revenue).to eq(63)
+        end
+
+        it 'another merchants with no discounts items are not being affected by discounts' do
+          merchant2 = create(:merchant)
+          item_3 = Item.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 10, merchant_id: merchant2.id, status: 1)
+          invoice_item3 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: item_3.id, quantity: 1, unit_price: 10, status: 1)
+          BulkDiscount.create!(percent_discounted: 50, quantity_threshold: 5, merchant_id: @merchant1.id)
+          expect(@invoice_1.discount_total_revenue).to eq(65)
+        end
       end
-      it 'gives a discounted total for a merchant, based on quantity of invoice items, and percent off' do
-        expect(@invoice_1.discount_total_revenue).to eq(50)
-      end
+    end
   end
 end
