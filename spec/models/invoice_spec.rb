5c24ce29a52bceb5b4f77ad82ed1fa15259144bb
Chris Crane <chrisjcrane@gmail.com>
2023-03-06 17:58:28 -0700

    create total_discounted_revenue method for invoice

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -19,17 +19,17 @@ RSpec.describe Invoice, type: :model do
     @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
     @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
     @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
-    @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
-    @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+    @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 2)
+    @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 5, unit_price: 10, status: 1, discount: 50)
   end
 
   describe "instance methods" do
     it "total_revenue" do
-      expect(@invoice_1.total_revenue).to eq(100)
+      expect(@invoice_1.total_revenue).to eq(150)
     end
 
     it "total_discounted_revenue" do
-
+      expect(@invoice_1.total_discounted_revenue).to eq(100)
     end
   end
 end
