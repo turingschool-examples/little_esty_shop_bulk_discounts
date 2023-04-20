8e3e79c610c6778917a9c45f01a77e24e2e5d25c
Isaac Thill <ithill22@turing.edu>
2023-04-20 17:58:41 -0600

    complete user story 1

--- db/migrate/20230420215141_create_bulk_discounts.rb
+++ db/migrate/20230420215141_create_bulk_discounts.rb
@@ -2,7 +2,7 @@ class CreateBulkDiscounts < ActiveRecord::Migration[5.2]
   def change
     create_table :bulk_discounts do |t|
       t.references :merchant, foreign_key: true
-      t.float :percentage_discout
+      t.float :percentage_discount
       t.integer :quantity_threshold
       t.string :promo_name
 
