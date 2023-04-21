25d4fd7b66f71f0045fcf25c90b68fca41db1673
Crow Rising <miscbyrd@gmail.com>
2023-04-21 15:52:24 -0600

    docs: add to factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
+  factory :bulk_discount do
+    percent_discount { 1.5 }
+    quantity_threshold { 1 }
+    merchant { nil }
+  end
+
   factory :customer do
     first_name {Faker::Name.first_name}
     last_name {Faker::Dessert.variety}
