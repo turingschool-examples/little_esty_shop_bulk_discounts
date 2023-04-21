63f6974f86a7aba13bea9f6640ef339cf2e7e37b
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-20 19:08:20 -0600

    Feat: finish US1 and bulk discount show page

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,2 +1,19 @@
 class BulkDiscountsController < ApplicationController
+  before_action :find_merchant, only: [:index, :new]
+
+  def index
+    @bulk_discounts = @merchant.bulk_discounts
+  end
+
+  def show
+    @bulk_discount = BulkDiscount.find(params[:id])
+    @merchant = @bulk_discount.merchant
+  end
+
+  def new
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
 end
