0019d2f995aac8a2fc38f4ce233d2d70c1d7bdab
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-21 13:09:26 -0600

    Deploy: update new credentials for render deployment

--- spec/features/bulk_discounts/new_spec.rb
+++ spec/features/bulk_discounts/new_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Bulk Discount New', type: :feature do
   before(:each) do
     @merchant_1 = Merchant.create!(name: 'Hair Care')
+
+    visit new_merchant_bulk_discount_path(@merchant_1)
   end
 
   describe 'User Story 2' do
+    it 'has a form to create a new bulk discount' do
+      expect(page).to have_field(:percentage_discount)
+      expect(page).to have_field(:quantity_threshold)
+    end
+    
     it 'when I fill in the form with valid data, I am redirected back to the bulk discounts index page'
     it 'and I see my new bulk discount listed'
 
