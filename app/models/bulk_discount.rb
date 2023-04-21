d0f19ad52f0e4fa89df9ee79abb5fb79d7e2e7a6
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-21 15:28:02 -0600

    Test: fix tests for numericality

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,8 +2,8 @@ class BulkDiscount < ApplicationRecord
   belongs_to :merchant
   has_many :items, through: :merchant
 
-  validates_presence_of :percentage_discount, numericality: { only_integer: true, greater_than: 0, less_than: 50 }
-  validates_presence_of :quantity_threshold, numericality: { only_integer: true }
+  validates_numericality_of :percentage_discount, { only_integer: true, greater_than: 0 }
+  validates_numericality_of :quantity_threshold, { only_integer: true, greater_than: 0 }
 
   def bulk_discount_merchant
     merchant
