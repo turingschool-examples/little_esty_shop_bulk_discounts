a48123ee831cd5ffdcadc921190005076b2249e7
Crow Rising <miscbyrd@gmail.com>
2023-04-21 16:36:55 -0600

    chore: move bulk discount route to merchant

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :bulk_discounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchant, only: [:show] do
     resources :dashboard, only: [:index]
     resources :items, except: [:destroy]
     resources :item_status, only: [:update]
     resources :invoices, only: [:index, :show, :update]
+    resources :bulk_discounts
   end
 
   namespace :admin do
