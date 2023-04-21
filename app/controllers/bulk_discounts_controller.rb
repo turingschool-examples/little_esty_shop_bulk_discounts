66650e2f56ddc025c4d205a5ba84ffc81388ef60
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-21 17:05:51 -0600

    Feat: create destroy action for bulk discounts controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,5 +1,5 @@
 class BulkDiscountsController < ApplicationController
-  before_action :find_merchant, only: [:index, :new, :create]
+  before_action :find_merchant, only: [:index, :new, :create, :destroy]
 
   def index
     @bulk_discounts = @merchant.find_bulk_discounts
@@ -23,6 +23,13 @@ class BulkDiscountsController < ApplicationController
 
       redirect_to new_merchant_bulk_discount_path(@merchant)
     end
+  end
+
+  def destroy
+    @bulk_discount = BulkDiscount.find(params[:id])
+    @bulk_discount.destroy
+
+    redirect_to merchant_bulk_discounts_path(@merchant)
   end
 
   def find_merchant
