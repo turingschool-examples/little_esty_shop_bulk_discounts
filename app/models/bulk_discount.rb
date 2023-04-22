bf80b8315588deacc01c7d1c4a7e42e3c278331b
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-22 11:11:51 -0600

    feat: Add validations.

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,6 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+  validates :name, presence: true
+  validates :discount_percent, presence: true
+  validates :quantity_threshold, presence: true
 end
