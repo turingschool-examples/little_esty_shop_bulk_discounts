2b75d1d8c4117e1ff1bcd2e7f2ebee36d2b7d139
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-22 11:10:42 -0600

    feat: Add validations.

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :discount_percent }
+    it { should validate_presence_of :quantity_threshold }
+  end
+
   describe 'relationships' do
     it { should belong_to :merchant }
   end
