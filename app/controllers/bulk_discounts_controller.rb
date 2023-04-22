6bbba7aa6d3f58daf1d11a900f9f17a576514fa4
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-22 13:39:02 -0600

    feat: Add show action and find_bulk_discount method to the controller.

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,4 +1,11 @@
 class BulkDiscountsController < ApplicationController
+  before_action :find_bulk_discount, only: [:show]
+  def show
+  end
 
-  
+  private
+
+  def find_bulk_discount
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
 end
