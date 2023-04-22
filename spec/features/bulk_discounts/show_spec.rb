e18279ddbdec8c6f0179080c04c2e8dc1d78d81e
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-22 13:34:46 -0600

    fix: Remove save and open page.

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe 'dashboard/bulk_discounts index spec'do
 
   describe 'As a merchant, when I visit my bulk discount show page' do
     it 'I see the bulk discount quantity threshold and percentage discount' do
-      save_and_open_page
       expect(page).to have_content(@bulk_disc1.name)
       expect(page).to have_content(@bulk_disc1.discount_percent.to_i)
       expect(page).to have_content(@bulk_disc1.quantity_threshold)
