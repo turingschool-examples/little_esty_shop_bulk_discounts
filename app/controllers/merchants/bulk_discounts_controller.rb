be6260c0701f9a7ddbade114da267aad39ee3fa7
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-22 11:14:58 -0600

    feat: Add new and creat action to controller.

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -3,4 +3,26 @@ class Merchants::BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = BulkDiscount.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.bulk_discounts.new(bulk_discount_params)
+    if @discount.save
+      redirect_to merchant_bulk_discounts_path(@merchant)
+    else
+      flash[:error] = "Please fill in all fields, did you not read?"
+      render :new
+    end
+  end
+  
+  private
+
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:name, :discount_percent, :quantity_threshold)
+  end
 end
