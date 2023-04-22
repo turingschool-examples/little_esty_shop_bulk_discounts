e04ce788ceb673a10b2dec6aa7ad14928201b378
Crow Rising <miscbyrd@gmail.com>
2023-04-21 20:50:19 -0600

    test: add tests for validations

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :percent_discount }
+    it { should validate_numericality_of :percent_discount }
+    it { should validate_presence_of :quantity_threshold }
+    it { should validate_numericality_of :quantity_threshold }
+  end
+
   describe 'relationships' do
     it { should belong_to :merchant }
   end
