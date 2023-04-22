bb90b0033100ab003aa726dfcca4f3860b3c29c1
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-22 13:33:35 -0600

    test: Add tests for displaying bulk discount info.

---
+++ spec/features/bulk_discounts/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'dashboard/bulk_discounts index spec'do
+  before :each do
+    test_data
+    visit bulk_discount_path(@bulk_disc1)
+  end
+
+  describe 'As a merchant, when I visit my bulk discount show page' do
+    it 'I see the bulk discount quantity threshold and percentage discount' do
+      save_and_open_page
+      expect(page).to have_content(@bulk_disc1.name)
+      expect(page).to have_content(@bulk_disc1.discount_percent.to_i)
+      expect(page).to have_content(@bulk_disc1.quantity_threshold)
+    end
+  end
+end
