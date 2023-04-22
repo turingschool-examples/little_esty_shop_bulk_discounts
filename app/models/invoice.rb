ea8a3196fdcd116ec2d3b9da6c9594d2db6a9640
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-22 16:39:41 -0600

    Feat: start working on AR for total_discount_revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -13,4 +13,11 @@ class Invoice < ApplicationRecord
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+  def total_discounted_revenue
+    invoice_items.joins(item: { merchant: :bulk_discounts })
+    .where("invoice_items.quantity >= bulk_discounts.quantity_threshold")
+    
+    require 'pry'; binding.pry
+  end
 end
