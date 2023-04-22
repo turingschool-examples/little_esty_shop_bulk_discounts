3eb1a5ba41762a78911d13218b40ff9faacbceca
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-22 11:13:34 -0600

    test: Add merchants bulk discounts show page test environment.

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe 'dashboard/bulk_discounts index spec'do
     visit bulk_discount_path(@bulk_disc1)
   end
 
-  describe 'As a merchant, when I visit my bulk discount show page' do
-    it 'has discount name' do
-      expect(page).to have_content(@bulk_disc1.name)
-    end
-  end
+  # describe 'As a merchant, when I visit my bulk discount show page' do
+  #   it 'has discount name' do
+  #     expect(page).to have_content(@bulk_disc1.name)
+  #   end
+  # end
 end
