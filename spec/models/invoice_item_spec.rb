c806c0400073653362d8850f9c03eb7803285399
Crow Rising <miscbyrd@gmail.com>
2023-04-25 11:23:40 -0600

    test: made test for find discount method

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -35,9 +35,40 @@ RSpec.describe InvoiceItem, type: :model do
       @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 1, unit_price: 8, status: 0)
       @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
       @ii_4 = InvoiceItem.create!(invoice_id: @i3.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 1)
+      
     end
     it 'incomplete_invoices' do
       expect(InvoiceItem.incomplete_invoices).to eq([@i1, @i3])
+    end
+  end
+
+  describe 'new class method find_discount' do
+    before(:each) do
+      @merchant1 = Merchant.create!(name: 'Hair Care')
+      @merchant2 = Merchant.create!(name: 'Car Decals')
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+      @item_9 = Item.create!(name: "Rainbow", description: "Virtue signal sticker for your back window", unit_price: 5, merchant_id: @merchant1.id)
+      @item_10 = Item.create!(name: "Barbed wire", description: "Tell the world you are tough", unit_price: 5, merchant_id: @merchant1.id)
+      @item_11 = Item.create!(name: "You name for pres", description: "Everyone wants to vote for you", unit_price: 5, merchant_id: @merchant2.id)
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @customer_2 = Customer.create!(first_name: 'Geraldine', last_name: 'Jones')
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      @invoice_2 = Invoice.create!(customer_id: @customer_2.id, status: 2, created_at: "2012-03-27 14:54:09")
+
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 10, unit_price: 10, status: 2)
+      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 12, unit_price: 10, status: 1)
+      @ii_12 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_9.id, quantity: 1, unit_price: 10, status: 1)
+      @ii_13 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_11.id, quantity: 5, unit_price: 10, status: 1)
+
+      @bulk_discount_1 = @merchant1.bulk_discounts.create!(percent_discount: 15, quantity_threshold: 10)
+      @bulk_discount_2 = @merchant1.bulk_discounts.create!(percent_discount: 25, quantity_threshold: 12)
+      @bulk_discount_3 = @merchant2.bulk_discounts.create!(percent_discount: 10, quantity_threshold: 5)
+    end
+    
+    it 'has a method find the bulk_discount applied to invoice item' do
+
+      expect(@ii_1.find_discount).to eq(@bulk_discount_1)
     end
   end
 end
