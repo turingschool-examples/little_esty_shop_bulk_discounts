81172fb156a011e507f34e3a29a81ea860315c44
Logan Wilson <loganwilson777@gmail.com>
2023-04-25 22:59:56 -0600

    Feat: bulk discounts controller

---
+++ app/controllers/bulk_discounts_controller.rb
@@ -0,0 +1,16 @@
+class BulkDiscountsController < ApplicationController
+  def index 
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discounts = @merchant.bulk_discounts
+  end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+
+  private 
+  def bulk_discount_params
+    params.permit(:percentage_discount, :quantity_threshold, :promo_name, :merchant_id)
+  end
+end
